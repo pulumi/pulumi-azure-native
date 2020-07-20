adfdba122062c16a91cc995b4d018d96d3b458aa
Pat Gavlin <pat@pulumi.com>
2020-07-20 12:22:29 -0700

    PR feedback

--- provider/cmd/pulumi-resource-azurerm/main.go
+++ provider/cmd/pulumi-resource-azurerm/main.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate go run ./generate.go ../../../sdk/schema/schema.json
-
 package main
 
 import (
