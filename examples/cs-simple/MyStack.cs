5933619362931aa1cbc2342a0ea600519bec7a75
stack72 <public@paulstack.co.uk>
2020-08-28 22:19:05 +0100

    Adding a DotNet Acceptance Test

--- examples/cs-simple/MyStack.cs
+++ examples/cs-simple/MyStack.cs
@@ -2,21 +2,29 @@ using Pulumi;
 using Pulumi.AzureRM.Resources.V20200601;
 using Pulumi.AzureRM.Storage.V20190601;
 using Pulumi.AzureRM.Storage.V20190601.Inputs;
+using Pulumi.Random;
 
 class MyStack : Stack
 {
     public MyStack()
     {
+        var randomString = new RandomString("random", new RandomStringArgs
+        {
+            Length = 12,
+            Special = false,
+            Upper = false,
+        });
+
         var resourceGroup = new ResourceGroup("resourceGroup", new ResourceGroupArgs
         {
-            ResourceGroupName = "azurerm-dotnet",
+            ResourceGroupName = randomString.Result,
             Location = "WestUS"
         });
 
         var storageAccount = new StorageAccount("sa", new StorageAccountArgs
         {
             ResourceGroupName = resourceGroup.Name,
-            AccountName = "pulumiassacs",
+            AccountName = randomString.Result,
             Location = "WestUS",
             Sku = new SkuArgs
             {
