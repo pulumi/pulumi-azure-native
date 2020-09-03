fcb272edb1aa4c92c2621db1182c645cdc8182ba
Mikhail Shilkov <github@mikhail.io>
2020-09-03 12:32:06 +0200

    Add to tests

--- examples/examples_dotnet_test.go
+++ examples/examples_dotnet_test.go
@@ -10,6 +10,15 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
+func TestAccAppServiceDockerDotnet(t *testing.T) {
+	test := getCsharpBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: filepath.Join(getCwd(t), "cs-appservice-docker"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
 func TestAccSimpleDotnet(t *testing.T) {
 	test := getCsharpBaseOptions(t).
 		With(integration.ProgramTestOptions{
@@ -18,6 +27,7 @@ func TestAccSimpleDotnet(t *testing.T) {
 
 	integration.ProgramTest(t, &test)
 }
+
 
 func getCsharpBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions(t)
