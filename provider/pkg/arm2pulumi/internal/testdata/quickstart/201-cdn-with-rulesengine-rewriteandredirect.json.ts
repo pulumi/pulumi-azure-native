696632dfc533656ba6ab13717e8c5721df052b46
Vivek Lakshmanan <vivek@pulumi.com>
2020-12-09 11:34:30 -0800

    Test changes

---
+++ provider/pkg/arm2pulumi/internal/testdata/quickstart/201-cdn-with-rulesengine-rewriteandredirect.json.ts
@@ -0,0 +1,88 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure_nextgen from "@pulumi/azure-nextgen";
+
+const config = new pulumi.Config();
+const cDNSkuParam = config.get("cDNSkuParam") || "Standard_Microsoft";
+const endpointNameParam = config.require("endpointNameParam");
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
+const resourceGroupVar = azure_nextgen.resources.latest.getResourceGroup({
+    resourceGroupName: resourceGroupNameParam,
+});
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
+const originUrlParam = config.require("originUrlParam");
+const profileNameParam = config.require("profileNameParam");
+const profileResource = new azure_nextgen.cdn.v20190415.Profile("profileResource", {
+    location: locationParam,
+    profileName: profileNameParam,
+    resourceGroupName: resourceGroupNameParam,
+    sku: {
+        name: cDNSkuParam,
+    },
+});
+const endpointResource = new azure_nextgen.cdn.v20190415.Endpoint("endpointResource", {
+    contentTypesToCompress: [
+        "text/plain",
+        "text/html",
+        "text/css",
+        "application/x-javascript",
+        "text/javascript",
+    ],
+    deliveryPolicy: {
+        description: "Rewrite and Redirect",
+        rules: [
+            {
+                actions: [{
+                    name: "UrlRewrite",
+                    parameters: {
+                        destination: "/mobile",
+                        odataType: "#Microsoft.Azure.Cdn.Models.DeliveryRuleUrlRewriteActionParameters",
+                        sourcePattern: "/standard",
+                    },
+                }],
+                conditions: [{
+                    name: "IsDevice",
+                    parameters: {
+                        matchValues: ["Mobile"],
+                        odataType: "#Microsoft.Azure.Cdn.Models.DeliveryRuleIsDeviceConditionParameters",
+                        operator: "Equal",
+                    },
+                }],
+                name: "PathRewriteBasedOnDeviceMatchCondition",
+                order: "1",
+            },
+            {
+                actions: [{
+                    name: "UrlRedirect",
+                    parameters: {
+                        destinationProtocol: "Https",
+                        odataType: "#Microsoft.Azure.Cdn.Models.DeliveryRuleUrlRedirectActionParameters",
+                        redirectType: "Found",
+                    },
+                }],
+                conditions: [{
+                    name: "RequestScheme",
+                    parameters: {
+                        matchValues: ["HTTP"],
+                        odataType: "#Microsoft.Azure.Cdn.Models.DeliveryRuleRequestSchemeConditionParameters",
+                        operator: "Equal",
+                    },
+                }],
+                name: "HttpVersionBasedRedirect",
+                order: "2",
+            },
+        ],
+    },
+    endpointName: `${profileNameParam}/${endpointNameParam}`,
+    isCompressionEnabled: true,
+    isHttpAllowed: true,
+    isHttpsAllowed: true,
+    location: locationParam,
+    originHostHeader: originUrlParam,
+    origins: [{
+        hostName: originUrlParam,
+        name: "origin1",
+    }],
+    profileName: profileResource.name,
+    queryStringCachingBehavior: "IgnoreQueryString",
+    resourceGroupName: resourceGroupNameParam,
+});
---
+++ provider/pkg/arm2pulumi/internal/testdata/quickstart/301-insights-alertrules-application-insights.json.ts
@@ -0,0 +1,66 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure_nextgen from "@pulumi/azure-nextgen";
+
+const config = new pulumi.Config();
+const applicationInsightsNameParam = config.get("applicationInsightsNameParam") || "myApplicationInsights";
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
+const resourceGroupVar = azure_nextgen.resources.latest.getResourceGroup({
+    resourceGroupName: resourceGroupNameParam,
+});
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
+const workspaceNameParam = config.get("workspaceNameParam") || "myWorkspace";
+const workspaceResource = new azure_nextgen.operationalinsights.v20200301preview.Workspace("workspaceResource", {
+    location: locationParam,
+    resourceGroupName: resourceGroupNameParam,
+    sku: {
+        name: "Free",
+    },
+    workspaceName: workspaceNameParam,
+});
+const componentResource = new azure_nextgen.insights.v20200202preview.Component("componentResource", {
+    applicationType: "web",
+    kind: "web",
+    location: locationParam,
+    resourceGroupName: resourceGroupNameParam,
+    resourceName: applicationInsightsNameParam,
+    workspaceResourceId: workspaceResource.id,
+});
+const emailActionGroup = new azure_nextgen.insights.v20190601.ActionGroup("emailActionGroup", {
+    actionGroupName: "emailActionGroup",
+    emailReceivers: [{
+        emailAddress: "example@test.com",
+        name: "Example",
+        useCommonAlertSchema: true,
+    }],
+    enabled: true,
+    groupShortName: "string",
+    location: "global",
+    resourceGroupName: resourceGroupNameParam,
+});
+const responseAlertNameVar = "[concat('ResponseTime-', toLower(parameters('applicationInsightsName')))]";
+const responseTimeParam = config.getNumber("responseTimeParam") || 3;
+const metricAlertResource = new azure_nextgen.insights.v20180301.MetricAlert("metricAlertResource", {
+    actions: [{
+        actionGroupId: emailActionGroup.id,
+    }],
+    criteria: {
+        allOf: [{
+            criterionType: "StaticThresholdCriterion",
+            metricName: "requests/duration",
+            name: "1st criterion",
+            operator: "GreaterThan",
+            threshold: responseTimeParam,
+            timeAggregation: "Average",
+        }],
+        odataType: "Microsoft.Azure.Monitor.SingleResourceMultipleMetricCriteria",
+    },
+    description: "response time alert",
+    enabled: true,
+    evaluationFrequency: "PT1M",
+    location: "global",
+    resourceGroupName: resourceGroupNameParam,
+    ruleName: responseAlertNameVar,
+    scopes: [componentResource.id],
+    severity: 0,
+    windowSize: "PT5M",
+});
