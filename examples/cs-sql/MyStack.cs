8643be495847dca179a118d7797108cdc15d2557
Mikhail Shilkov <github@mikhail.io>
2021-01-21 12:06:53 +0100

    Relax the check of resource existence to fit the behavior of private dns zones

--- examples/cs-sql/MyStack.cs
+++ examples/cs-sql/MyStack.cs
@@ -1,4 +1,7 @@
 using Pulumi;
+using Pulumi.AzureNextGen.Network.Latest;
+using Pulumi.AzureNextGen.Network.Latest.Inputs;
+using SubnetArgs = Pulumi.AzureNextGen.Network.Latest.Inputs.SubnetArgs;
 using Pulumi.AzureNextGen.Resources.Latest;
 using Pulumi.AzureNextGen.Sql.Latest;
 using Pulumi.Random;
@@ -30,13 +33,54 @@ class MyStack : Stack
             Version = "12.0"
         });
         
-        var sqlFwRuleClientIP2 = new FirewallRule("sqlFwRuleClientIP", new FirewallRuleArgs
+        var sqlFwRuleClientIP = new FirewallRule("sqlFwRuleClientIP", new FirewallRuleArgs
         {
             ResourceGroupName = resourceGroup.Name,
             ServerName = server.Name,
             FirewallRuleName = "ClientIPAddress",
             StartIpAddress = "222.222.222.222",
             EndIpAddress = "222.222.222.222"
+        });
+        
+        var vnet = new VirtualNetwork("vnet", new VirtualNetworkArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            VirtualNetworkName = randomString.Result,
+            Location = resourceGroup.Location,
+            AddressSpace = new AddressSpaceArgs { AddressPrefixes = {"10.1.0.0/16"} },
+            Subnets = 
+            {
+                new SubnetArgs
+                {
+                    Name = "default",
+                    AddressPrefix = "10.1.0.0/24",
+                    PrivateEndpointNetworkPolicies = "Disabled"
+                }
+            }
+        });
+
+        var privateEndpoint = new PrivateEndpoint("endpoint", new PrivateEndpointArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            PrivateEndpointName = randomString.Result,
+            Location = resourceGroup.Location,
+            PrivateLinkServiceConnections =
+            {
+                new PrivateLinkServiceConnectionArgs
+                {
+                    GroupIds = {"sqlServer"},
+                    PrivateLinkServiceId = server.Id,
+                    Name = "conn-sql",
+                }
+            },
+            Subnet = new SubnetArgs { Id = vnet.Subnets.GetAt(0).Apply(v => v.Id) }
+        });
+
+        var privateDnsZone = new PrivateDnsZoneGroup("zoneGroup", new PrivateDnsZoneGroupArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            PrivateDnsZoneGroupName = randomString.Result,
+            PrivateEndpointName = privateEndpoint.Name,
         });
     }
 }
