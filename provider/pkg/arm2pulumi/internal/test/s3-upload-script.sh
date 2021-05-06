3f837b5e64d5c6911290ec310036b087f92edf41
Hitesh Boinpally <hiteshpulumi@Hiteshs-MacBook-Pro.local>
2021-05-06 15:25:55 -0700

    updated with new S3 bucket name

--- provider/pkg/arm2pulumi/internal/test/s3-upload-script.sh
+++ provider/pkg/arm2pulumi/internal/test/s3-upload-script.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 currDateTime=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Makes sure that each summary has a unique name in S3
 newSummaryName="summary_${currDateTime}.json"
 echo "New file to be uploaded: ${newSummaryName}"
 
+# URI of S3 bucket and location to upload to
+s3KeyName="s3://arm2pulumi-coverage-results-236fe75/summaries/${newSummaryName}"
+
 cd test-results
+# Edit JSON summary file to be copiable into Redshift
+# Changing file from a list of JSON objects to group of JSON objects. Done by 
+# removing first ("[") and last line ("]") and replacing all "}," with "}".
 sed '1d' summary.json | sed '$d' | sed -E 's/},/}/' > $newSummaryName
-aws s3 cp $newSummaryName s3://temp-test-data-bucket-3b9ef45/summaries/$newSummaryName
+aws s3 cp $newSummaryName $s3KeyName
