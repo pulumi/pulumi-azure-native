151eb7791e3a10706fc29814c4efb1ff90f86105
Hitesh Boinpally <hiteshpulumi@Hiteshs-MacBook-Pro.local>
2021-05-11 16:18:09 -0700

    updated script to give ownership to bucket

--- provider/pkg/arm2pulumi/internal/test/s3-upload-script.sh
+++ provider/pkg/arm2pulumi/internal/test/s3-upload-script.sh
@@ -6,12 +6,16 @@ currDateTime=$(date +"%Y-%m-%d_%H-%M-%S")
 newSummaryName="summary_${currDateTime}.json"
 echo "New file to be uploaded: ${newSummaryName}"
 
-# URI of S3 bucket and location to upload to
-s3KeyName="s3://arm2pulumi-coverage-results-d8adb2a/summaries/${newSummaryName}"
+# Sets up URI of S3 bucket and location to upload to
+s3BucketName="arm2pulumi-coverage-results-d8adb2a"
+s3KeyName="summaries/${newSummaryName}"
+s3FullURI="s3://${s3BucketName}/${s3KeyName}"
 
 cd test-results
 # Edit JSON summary file to be copiable into Redshift
 # Changing file from a list of JSON objects to group of JSON objects. Done by 
 # removing first ("[") and last line ("]") and replacing all "}," with "}".
+# Modified JSON contents are then copied into the file `newSummaryName`.
 sed '1d' summary.json | sed '$d' | sed -E 's/},/}/' > $newSummaryName
-aws s3 cp $newSummaryName $s3KeyName
+
+aws s3 cp $newSummaryName $s3FullURI --acl bucket-owner-full-control
