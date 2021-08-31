ee7a19e029e5cf85d0fff723c2f361f80b2c3048
Levi Blackstone <levi@pulumi.com>
2021-08-31 14:27:11 -0600

    Fix outdated Go example (#1100)

--- examples/go-simple/main.go
+++ examples/go-simple/main.go
@@ -3,10 +3,10 @@
 package main
 
 import (
-	resources "github.com/pulumi/pulumi-azure-nextgen/sdk/go/azure/resources/latest"
-	storage "github.com/pulumi/pulumi-azure-nextgen/sdk/go/azure/storage/latest"
-	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	resources "github.com/pulumi/pulumi-azure-native/sdk/go/azure/resources"
+	storage "github.com/pulumi/pulumi-azure-native/sdk/go/azure/storage"
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
