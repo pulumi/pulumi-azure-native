a9af61d9f65d94a7be43235f9a46c451e4115b77
Daniel Bradley <daniel@pulumi.com>
2022-06-21 18:00:08 +0100

    Spike automatic curation of a new default version

--- provider/cmd/pulumi-versioner-azure-native/main.go
+++ provider/cmd/pulumi-versioner-azure-native/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"os"
 	"path"
+	"sort"
+	"time"
 )
 
 func main() {
@@ -51,6 +53,7 @@ func writeAll(outputDir string) error {
 		"deprecated.json":     deprecated,
 		"active.json":         activePathVersionsJson,
 		"pending.json":        pending,
+		"v2-config.json":      buildSpecs(specVersions),
 	})
 }
 
@@ -90,4 +93,98 @@ func emitFile(outPath string, contents []byte) error {
 
 	_, err = f.Write(contents)
 	return err
+}
+
+type ProviderSpec struct {
+	NoVersion     *bool                                          `json:"NoVersion,omitempty"`
+	SingleVersion *openapi.ApiVersion                            `json:"SingleVersion,omitempty"`
+	Rollup        *map[openapi.ApiVersion][]openapi.ResourceName `json:"Rollup,omitempty"`
+}
+
+func buildSpecs(spec versioning.SpecVersions) map[openapi.ProviderName]ProviderSpec {
+	specs := map[openapi.ProviderName]ProviderSpec{}
+	for providerName, versionResources := range spec {
+		specs[providerName] = buildSpec(versionResources)
+	}
+	return specs
+}
+
+func buildSpec(versions versioning.VersionResources) ProviderSpec {
+	noVersion := true
+	latestVersions := findLatestVersions(versions)
+	switch len(latestVersions) {
+	case 0:
+		return ProviderSpec{
+			NoVersion: &noVersion,
+		}
+	case 1:
+		for apiVersion, _ := range latestVersions {
+			return ProviderSpec{
+				SingleVersion: &apiVersion,
+			}
+		}
+	}
+	return ProviderSpec{
+		Rollup: &latestVersions,
+	}
+}
+
+func findLatestVersions(versions versioning.VersionResources) map[openapi.ApiVersion][]openapi.ResourceName {
+	latestResourceVersions := findLatestResourceVersions(versions)
+	latestVersions := map[openapi.ApiVersion][]openapi.ResourceName{}
+	for resourceName, version := range latestResourceVersions {
+		latestVersions[version] = append(latestVersions[version], resourceName)
+	}
+	for _, resourceNames := range latestVersions {
+		sort.Strings(resourceNames)
+	}
+	return latestVersions
+}
+
+func findLatestResourceVersions(versions versioning.VersionResources) map[openapi.ResourceName]openapi.ApiVersion {
+	orderedVersions := make([]openapi.ApiVersion, 0, len(versions))
+	for version, _ := range versions {
+		orderedVersions = append(orderedVersions, version)
+	}
+	sort.Strings(orderedVersions)
+
+	latestResourceVersions := map[openapi.ResourceName]openapi.ApiVersion{}
+	for _, version := range orderedVersions {
+		resourceNames := versions[version]
+		for _, resourceName := range resourceNames {
+			latestVersion := latestResourceVersions[resourceName]
+			if latestVersion > version {
+				continue
+			}
+			if latestVersion == "" {
+				latestResourceVersions[resourceName] = version
+				continue
+			}
+
+			isToStable := !openapi.IsPreview(version)
+			isFromPreview := openapi.IsPreview(latestVersion)
+			timeSinceLastVersion, err := timeBetweenVersions(latestVersion, version)
+			if err != nil {
+				println("unable to parse version as time", latestVersion, version, err)
+				continue
+			}
+			isLatestOld := timeSinceLastVersion.Hours() > (2 * 365 * 24)
+			if isToStable || isFromPreview || isLatestOld {
+				latestResourceVersions[resourceName] = version
+			}
+		}
+	}
+	return latestResourceVersions
+}
+
+func timeBetweenVersions(from, to openapi.ApiVersion) (diff time.Duration, err error) {
+	fromTime, err := time.Parse("2006-01-02", from[:10])
+	if err != nil {
+		return diff, err
+	}
+	toTime, err := time.Parse("2006-01-02", to[:10])
+	if err != nil {
+		return diff, err
+	}
+	return toTime.Sub(fromTime), err
 }
