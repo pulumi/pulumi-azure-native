6963030569901ee7d6b51a692fc5dbb4963938bc
Thomas Kappler <tkappler@pulumi.com>
2022-09-08 11:56:02 -0700

    Add test for getClientConfig

---
+++ examples/go-clientconfig/main.go
@@ -0,0 +1,27 @@
+// Copyright 2022, Pulumi Corporation.  All rights reserved.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi-azure-native/sdk/go/azure/authorization"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		config, err := authorization.GetClientConfig(ctx)
+		if err != nil {
+			return err
+		}
+
+		// The managed identity (MSI) authentication method will not have an object id, but it's
+		// unlikely our tests run on Azure VMs with this configuration.
+		if config.ClientId == "" || config.ObjectId == "" || config.SubscriptionId == "" || config.TenantId == "" {
+			return fmt.Errorf("Expected client, object, subscription, and tenant ids in auth config %v", config)
+		}
+
+		return nil
+	})
+}
