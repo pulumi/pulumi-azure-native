b38a1a23cad5753f62bc92cfd3acb331ca15d49d
Thomas Kappler <tkappler@pulumi.com>
2022-10-03 10:39:11 -0700

    Factor details of auth better

--- provider/pkg/provider/provider.go
+++ provider/pkg/provider/provider.go
@@ -181,10 +181,7 @@ func (k *azureNativeProvider) Configure(ctx context.Context,
 	}
 	k.environment = env
 
-	// The ctx Context given by gRPC is request-scoped and will be canceled after this request. We
-	// need the authorizers to function across requests.
-	authCtx := context.Background()
-	tokenAuth, bearerAuth, err := k.getAuthorizers(authCtx, authConfig)
+	tokenAuth, bearerAuth, err := k.getAuthorizers(authConfig)
 	if err != nil {
 		return nil, errors.Wrap(err, "building authorizer")
 	}
@@ -1741,70 +1738,6 @@ func (k *azureNativeProvider) getAuthConfig() (*authentication.Config, error) {
 	}
 
 	return builder.Build()
-}
-
-func getAzVersion() (*goversion.Version, error) {
-	_, err := exec.LookPath("az")
-	if err != nil {
-		return nil, fmt.Errorf("could not find `az`: %w", err)
-	}
-
-	var azVersion struct {
-		Cli string `json:"azure-cli"`
-	}
-	err = runAzCmd(&azVersion, "version", "--output", "json")
-	if err != nil {
-		return nil, fmt.Errorf("could not determine az version: %w", err)
-	}
-
-	actual, err := goversion.NewVersion(azVersion.Cli)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse az version \"%q\": %w", azVersion.Cli, err)
-	}
-
-	return actual, nil
-}
-
-func assertAzVersion(version *goversion.Version) error {
-	const versionHint = `Please make sure that the Azure CLI is installed in a version either
-between 2.0.81 and 2.33, or at least 2.37 but less than 3.x; or configure another authentication
-method. See https://www.pulumi.com/registry/packages/azure-native/installation-configuration/#credentials
-for more information.`
-
-	// We need this version because it doesn't print the error of #1565
-	lowerOkRange := goversion.MustConstraints(goversion.NewConstraint(">=2.0.81, <2.34"))
-	upperOkRange := goversion.MustConstraints(goversion.NewConstraint(">=2.37.0, <3"))
-
-	if !lowerOkRange.Check(version) && !upperOkRange.Check(version) {
-		return fmt.Errorf("found incompatible az version %s. %s", version, versionHint)
-	}
-
-	return nil
-}
-
-// Run `az` with the given args and unmarshal the output (must be JSON) into the given target struct
-func runAzCmd(target interface{}, arg ...string) error {
-	var stderr bytes.Buffer
-	var stdout bytes.Buffer
-
-	cmd := exec.Command("az", arg...)
-
-	cmd.Stderr = &stderr
-	cmd.Stdout = &stdout
-
-	if err := cmd.Run(); err != nil {
-		err := fmt.Errorf("running az: %w", err)
-		if stdErrStr := stderr.String(); stdErrStr != "" {
-			err = fmt.Errorf("%s: %s", err, strings.TrimSpace(stdErrStr))
-		}
-		return err
-	}
-
-	if err := json.Unmarshal(stdout.Bytes(), target); err != nil {
-		return fmt.Errorf("unmarshaling the result of Azure CLI: %w", err)
-	}
-
-	return nil
 }
 
 func getAzVersion() (*goversion.Version, error) {
