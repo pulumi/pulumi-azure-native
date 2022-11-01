45bb3fcba7fc7db9d74d35a4ff75defbb72d4b68
Thomas Kappler <tkappler@pulumi.com>
2022-11-01 03:46:20 -0700

    Run ProgramTest acceptance tests on split Go SDKs (#2056)
    * Run integration tests on new split Go SDK as well.
    
    This version uses the same build tag, "go", so both classic and split
    tests always run together.
    
    * Require all SDK packages so we can't miss one
    
    * Support tests with nested directories
    
    * Add go mod replace for root SDK package
    
    * Need absolute path to the SDK replacement

---
+++ examples/azure-native-sdk/go-servicebus-recreate/step2/main.go
@@ -0,0 +1,40 @@
+// Copyright 2021, Pulumi Corporation.  All rights reserved.
+
+package main
+
+import (
+	"github.com/pulumi/pulumi-azure-native-sdk/resources"
+	"github.com/pulumi/pulumi-azure-native-sdk/servicebus"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an Azure Resource Group
+		resourceGroup, err := resources.NewResourceGroup(ctx, "resourceGroup", nil)
+		if err != nil {
+			return err
+		}
+
+		namespace, err := servicebus.NewNamespace(ctx, "ns", &servicebus.NamespaceArgs{
+			ResourceGroupName: resourceGroup.Name,
+		})
+		if err != nil {
+			return err
+		}
+
+		topic, err := servicebus.NewTopic(ctx, "topic", &servicebus.TopicArgs{
+			ResourceGroupName:          resourceGroup.Name,
+			NamespaceName:              namespace.Name,
+			RequiresDuplicateDetection: pulumi.Bool(true), // Changing this should trigger recreation otherwise will be rejected.
+		})
+		if err != nil {
+			return err
+		}
+
+		// Export the topic name
+		ctx.Export("topicName", topic.Name)
+
+		return nil
+	})
+}
---
+++ examples/examples_go_sdk_test.go
@@ -0,0 +1,114 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+//go:build go || all
+// +build go all
+
+package examples
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+)
+
+const sdkPath = "../sdk/pulumi-azure-native-sdk"
+
+func testDir(t *testing.T, testCaseDirs ...string) string {
+	segments := []string{getCwd(t), "azure-native-sdk"}
+	segments = append(segments, testCaseDirs...)
+	return filepath.Join(segments...)
+}
+
+func TestAccSimpleGoSdk(t *testing.T) {
+	test := getGoBaseOptionsSdk(t).
+		With(integration.ProgramTestOptions{
+			Dir: testDir(t, "go-simple"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccClientConfigGoSdk(t *testing.T) {
+	test := getGoBaseOptionsSdk(t).
+		With(integration.ProgramTestOptions{
+			Dir: testDir(t, "go-clientconfig"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAksGoSdk(t *testing.T) {
+	t.Skip("Disabled due to https://github.com/pulumi/pulumi-azure-native/issues/304")
+	test := getGoBaseOptionsSdk(t).
+		With(integration.ProgramTestOptions{
+			Dir: testDir(t, "go-aks"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestServicebusRecreateSdk(t *testing.T) {
+	test := getGoBaseOptionsSdk(t).
+		With(integration.ProgramTestOptions{
+			Dir: testDir(t, "go-servicebus-recreate", "step1"),
+			EditDirs: []integration.EditDir{
+				{
+					Dir:      testDir(t, "go-servicebus-recreate", "step2"),
+					Additive: true,
+				},
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getGoBaseOptionsSdk(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions(t)
+
+	required, err := getRequiredGoPackages()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rootSdkPath, err := filepath.Abs(sdkPath)
+	require.NoError(t, err)
+
+	dependencies := make([]string, len(required))
+	for i, pkg := range required {
+		dependencies[i] = genReplace(t, rootSdkPath, pkg)
+	}
+
+	dependencies = append(dependencies, "github.com/pulumi/pulumi-azure-native-sdk="+rootSdkPath)
+
+	baseGo := base.With(integration.ProgramTestOptions{
+		Dependencies: dependencies,
+	})
+
+	return baseGo
+}
+
+// We don't know which packages the tests need, so we just get all of them.
+func getRequiredGoPackages() ([]string, error) {
+	dirs, err := os.ReadDir(sdkPath)
+	if err != nil {
+		return nil, err
+	}
+
+	packages := make([]string, 0)
+	for _, entry := range dirs {
+		if entry.IsDir() {
+			packages = append(packages, entry.Name())
+		}
+	}
+
+	return packages, nil
+}
+
+// For context: https://github.com/pulumi/pulumi/pull/11055
+func genReplace(t *testing.T, sdkPath string, subPkg string) string {
+	moduleDir := fmt.Sprintf("%s/%s", sdkPath, subPkg)
+	return fmt.Sprintf("github.com/pulumi/pulumi-azure-native-sdk/%s=%s", subPkg, moduleDir)
+}
