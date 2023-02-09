327914a630e61cca629ffa5858436c536c4b9354
Thomas Kappler <tkappler@pulumi.com>
2023-02-09 14:31:32 -0800

    Fix oidc-only test build

--- examples/examples_nodejs_keyvault_test.go
+++ examples/examples_nodejs_keyvault_test.go
@@ -11,10 +11,23 @@ import (
 
 func TestAccKeyVaultTs(t *testing.T) {
 	skipIfShort(t)
-	test := getJSBaseOptions(t).
+	test := getJSBaseOptions2(t).
 		With(integration.ProgramTestOptions{
 			Dir: filepath.Join(getCwd(t), "keyvault"),
 		})
 
 	integration.ProgramTest(t, &test)
+}
+
+// A copy of getJSBaseOptions from examples_nodejs_test.go because that one
+// isn't built when testing with tag=oidc
+func getJSBaseOptions2(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions(t)
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"@pulumi/azure-native",
+		},
+	})
+
+	return baseJS
 }
