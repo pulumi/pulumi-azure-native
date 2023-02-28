659f706fb6c3b78809f41d5f4dc6f37c8bb60929
Daniel Bradley <daniel@pulumi.com>
2023-02-28 16:35:50 +0000

    Improve readability and testability of go tagging script
    - Extract to standalone script.
    - Break the big one-liner into multiple simpler steps.

---
+++ .github/scripts/tag-go-modules.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+test "${1}" = "" && echo "Usage: $0 <version>" && exit 1
+
+export VERSION=${1}
+
+# Tag root module
+git tag "v${VERSION}"
+
+# This script tags all go modules in the repository with the current version
+find . -name "go.mod" -type f -mindepth 2 -maxdepth 2 -exec sh -c '
+  DIR=$(dirname {$1})
+  BASE=$(basename $DIR)
+  TAG="${BASE}/v${VERSION}"
+  git tag "$TAG"
+  ' shell {} \;
