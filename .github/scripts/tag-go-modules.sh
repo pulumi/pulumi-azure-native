c3e4dd064fd6ec4408a960215471dc045682aead
Daniel Bradley <daniel@pulumi.com>
2023-02-28 20:02:54 +0000

    Simplify arguments

--- .github/scripts/tag-go-modules.sh
+++ .github/scripts/tag-go-modules.sh
@@ -9,7 +9,7 @@ git tag "v${VERSION}"
 
 # This script tags all go modules in the repository with the current version
 find . -name "go.mod" -type f -mindepth 2 -maxdepth 2 -exec sh -c '
-  DIR=$(dirname {$1})
+  DIR=$(dirname ${1})
   BASE=$(basename $DIR)
   TAG="${BASE}/v${VERSION}"
   git tag "$TAG"
