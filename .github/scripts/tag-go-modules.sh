1ca50e445f16b6a77c49586e485e8ef8df823074
Thomas Kappler <tkappler@pulumi.com>
2023-03-23 18:36:37 -0700

    Fix missing version in module tagging

--- .github/scripts/tag-go-modules.sh
+++ .github/scripts/tag-go-modules.sh
@@ -2,7 +2,8 @@
 
 test "${1}" = "" && echo "Usage: $0 <version>" && exit 1
 
-VERSION=${1}
+# needs to be exported so the subshells of the find command can see it
+export VERSION=${1}
 
 # Tag root module
 git tag "v${VERSION}"
