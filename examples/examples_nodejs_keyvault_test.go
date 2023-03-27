f1a44f8ea623991d015aa4b5a2989398cefcddbb
Thomas Kappler <tkappler@pulumi.com>
2023-03-27 14:19:39 -0700

    Review follow-up: use OIDC_ARM_CLIENT_ID

--- examples/examples_nodejs_keyvault_test.go
+++ examples/examples_nodejs_keyvault_test.go
@@ -22,18 +22,19 @@ func TestAccKeyVaultTs(t *testing.T) {
 
 func TestAccKeyVaultTs_OICD(t *testing.T) {
 	skipIfShort(t)
-	if os.Getenv("RUN_OIDC_TESTS") != "true" {
-		t.Skip("Skipping OIDC test outside of CI")
+
+	oidcClientId := os.Getenv("OIDC_ARM_CLIENT_ID")
+	if oidcClientId == "" {
+		t.Skip("Skipping OIDC test without OIDC_ARM_CLIENT_ID")
 	}
 	test := getJSBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir: filepath.Join(getCwd(t), "keyvault"),
 			Env: []string{
 				"ARM_USE_OIDC=true",
+				"ARM_CLIENT_ID=" + oidcClientId,
 				// not strictly necessary but making sure we test the OIDC path
 				"ARM_CLIENT_SECRET=",
-				// AD app 'oidc-test', owned by tkappler - temporary until our main app is OIDC-enabled
-				"ARM_CLIENT_ID=89380e12-5be6-486a-89ef-eea107af2f47",
 			},
 		})
 
