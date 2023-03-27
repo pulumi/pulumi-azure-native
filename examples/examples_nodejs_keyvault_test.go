58066d1269028afe0513e3e5a0a21085853cec78
Thomas Kappler <tkappler@pulumi.com>
2023-03-27 09:42:29 -0700

    Simplify OIDC test runs in CI

--- examples/examples_nodejs_keyvault_test.go
+++ examples/examples_nodejs_keyvault_test.go
@@ -1,17 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-//go:build nodejs || (nodejs && oidc) || all
+//go:build nodejs || all
 
 package examples
 
 import (
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
 func TestAccKeyVaultTs(t *testing.T) {
 	skipIfShort(t)
-	test := getJSBaseOptions2(t).
+	test := getJSBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir: filepath.Join(getCwd(t), "keyvault"),
 		})
@@ -19,15 +20,22 @@ func TestAccKeyVaultTs(t *testing.T) {
 	integration.ProgramTest(t, &test)
 }
 
-// A copy of getJSBaseOptions from examples_nodejs_test.go because that one
-// isn't built when testing with tag=oidc
-func getJSBaseOptions2(t *testing.T) integration.ProgramTestOptions {
-	base := getBaseOptions(t)
-	baseJS := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-			"@pulumi/azure-native",
-		},
-	})
+func TestAccKeyVaultTs_OICD(t *testing.T) {
+	skipIfShort(t)
+	if os.Getenv("RUN_OIDC_TESTS") != "true" {
+		t.Skip("Skipping OIDC test outside of CI")
+	}
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: filepath.Join(getCwd(t), "keyvault"),
+			Env: []string{
+				"ARM_USE_OIDC=true",
+				// not strictly necessary but making sure we test the OIDC path
+				"ARM_CLIENT_SECRET=",
+				// AD app 'oidc-test', owned by tkappler - temporary until our main app is OIDC-enabled
+				"ARM_CLIENT_ID=89380e12-5be6-486a-89ef-eea107af2f47",
+			},
+		})
 
-	return baseJS
+	integration.ProgramTest(t, &test)
 }
