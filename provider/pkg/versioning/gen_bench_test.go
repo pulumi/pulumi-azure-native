85784ff1c5cf432a77cbaa65f1176323e163e4aa
Daniel Bradley <daniel@pulumi.com>
2023-04-06 16:52:42 +0100

    Enable full benchmark test & document how to run

--- provider/pkg/versioning/gen_bench_test.go
+++ provider/pkg/versioning/gen_bench_test.go
@@ -8,34 +8,32 @@ import (
 	"github.com/pulumi/pulumi-azure-native/provider/pkg/openapi"
 )
 
+// BenchmarkGen benchmarks the generation of the Pulumi schema.
+// Run by executing `go test -benchmem -run=^$ -tags all -bench ^BenchmarkGen github.com/pulumi/pulumi-azure-native/provider/pkg/versioning -memprofile=gen.mem.pprof` in the provider folder.
+// View results with `go tool pprof -http=: gen.mem.pprof`
 func BenchmarkGen(b *testing.B) {
 	b.ReportAllocs()
 
 	rootDir := path.Join("..", "..", "..")
+
+	b.ResetTimer()
 
 	versionSources, err := ReadVersionSources(rootDir)
 	if err != nil {
 		b.Fatal(err)
 	}
 
-	b.ResetTimer()
-
-	// Test each provider
-	for k := range versionSources.v2Spec {
-		specs, err := openapi.ReadAzureProviders(path.Join(rootDir, "azure-rest-api-specs"), k)
-		if err != nil {
-			b.Fatal(err)
-		}
-
-		versionMetadata, err := calculateVersionMetadata(versionSources, specs)
-		if err != nil {
-			b.Fatal(err)
-		}
-
-		specs = openapi.ApplyProvidersTransformations(specs, versionMetadata.V2Lock, nil, versionMetadata.Deprecated, map[string][]string{})
-
-		// for i := 0; i < b.N; i++ {
-		gen.PulumiSchema(specs)
-		// }
+	specs, err := openapi.ReadAzureProviders(path.Join(rootDir, "azure-rest-api-specs"), "*")
+	if err != nil {
+		b.Fatal(err)
 	}
+
+	versionMetadata, err := calculateVersionMetadata(versionSources, specs)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	specs = openapi.ApplyProvidersTransformations(specs, versionMetadata.V2Lock, nil, versionMetadata.Deprecated, map[string][]string{})
+
+	gen.PulumiSchema(specs)
 }
