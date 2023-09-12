8cc76bd1aef30388049e43865f225d63aa4adabc
Daniel Bradley <daniel@pulumi.com>
2023-09-12 17:43:31 +0100

    Test end to end network subnet schema and metadata generation
    - Wrap whole process before we start making changes to the metadata so we can see the effect of our changes.
    - Take a copy of the relevant specification so it doesn't break the test on future schema changes.
    - Add instructions for when we want to update the test data snapshot.

---
+++ provider/pkg/gen/gen_vnet_test.go
@@ -0,0 +1,35 @@
+package gen
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/pulumi/pulumi-azure-native/v2/provider/pkg/openapi"
+)
+
+func TestVnetGen(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We take a snapshot of the schema as we're focusing on testing how we parse the spec
+	// To update the specifications run in the repo root:
+	// 1. rm -rf provider/pkg/gen/test-data/vnet/azure-rest-api-specs/specification/network/resource-manager/Microsoft.Network/stable/2023-02-01/
+	// 2. cp -r azure-rest-api-specs/specification/network/resource-manager/Microsoft.Network/stable/2023-02-01 provider/pkg/gen/test-data/vnet/azure-rest-api-specs/specification/network/resource-manager/Microsoft.Network/stable/
+	// 3. rm -rf provider/pkg/gen/test-data/vnet/azure-rest-api-specs/specification/network/resource-manager/Microsoft.Network/stable/2023-02-01/examples/
+	rootDir := path.Join(wd, "test-data", "vnet")
+
+	providers, _, err := openapi.ReadAzureProviders(path.Join(rootDir, "azure-rest-api-specs"), "Network", "2023-02-01")
+	if err != nil {
+		t.Fatal(err)
+	}
+	generationResult, err := PulumiSchema(providers, versioningStub{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	snaps.MatchSnapshot(t, generationResult.Schema)
+
+	snaps.MatchSnapshot(t, generationResult.Metadata)
+}
