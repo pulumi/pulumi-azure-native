7bc4b8ca3dd52b9edda07105968d1bb2085914c9
Thomas Kappler <tkappler@pulumi.com>
2023-12-05 11:24:04 +0100

    Move manually specified default properties to separate file and accessor method

---
+++ provider/pkg/gen/propertyDefaults.go
@@ -0,0 +1,20 @@
+package gen
+
+// In some cases, we need to set default values for properties that are not given as inputs.
+// One case is when the property was set and is then removed from the input, but Azure treats
+// the omission as "no change" instead of removing the value.
+// propertyDefaults returns a map of property names to default values for the given module
+// and resource, for recording in metadata for later use.
+func propertyDefaults(module, resourceName string) map[string]interface{} {
+	if resourceName == "StorageAccount" {
+		return map[string]interface{}{
+			"networkRuleSet": map[string]interface{}{
+				"bypass":              "AzureServices",
+				"defaultAction":       "Allow",
+				"ipRules":             []interface{}{},
+				"virtualNetworkRules": []interface{}{},
+			},
+		}
+	}
+	return nil
+}
