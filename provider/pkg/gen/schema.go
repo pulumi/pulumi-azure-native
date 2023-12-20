e1ae70ef6fed7bd3c97ca51095ceee2f18ecfb93
Thomas Kappler <tkappler@pulumi.com>
2023-12-20 20:11:22 +0100

    Fix path handling and simplify

--- provider/pkg/gen/schema.go
+++ provider/pkg/gen/schema.go
@@ -880,19 +880,11 @@ func (g *packageGenerator) genResourceVariant(apiSpec *openapi.ResourceSpec, res
 		DefaultProperties:    propertyDefaults(module, resource.typeName),
 	}
 
-	r.CollectSubResourceToMaintainIfUnset(g.lookupType)
-
 	g.metadata.Resources[resourceTok] = r
 
 	g.generateExampleReferences(resourceTok, path, swagger)
 	g.skippedForceNewTypes = append(g.skippedForceNewTypes, gen.skippedForceNewTypes...)
 	return nil
-}
-
-func (g *packageGenerator) lookupType(ref string) (*resources.AzureAPIType, bool, error) {
-	refTypeName := strings.TrimPrefix(ref, "#/types/")
-	t, ok := g.metadata.Types[refTypeName]
-	return &t, ok, nil
 }
 
 func (g *packageGenerator) generateAliases(resource *resourceVariant, typeNameAliases ...string) []pschema.AliasSpec {
--- provider/pkg/resources/resources.go
+++ provider/pkg/resources/resources.go
@@ -113,14 +113,6 @@ type AzureAPIResource struct {
 	RequiredContainers [][]string `json:"requiredContainers,omitempty"`
 	// Default values to be used when the property is removed or in importing. Must be top-level properties.
 	DefaultProperties map[string]interface{} `json:"defaultProperties,omitempty"`
-	// Some resources are nested beneath other resources, but are also made available as a property on the parent.
-	// When updating the parent, if the list of nested resources is not specified as a property on the parent, the child resources will be deleted.
-	// When refreshing the parent, the list of nested resources will be populated from the current state - resulting in an unwanted diff.
-	// We work around this by adding these properties containing a list of nested resources to ApiPathsToSubResourcesToMaintainIfUnset.
-	// When updating or refreshing the parent, if the list of nested resources was not originally specified inline, we'll maintain the existing sub-resources transparently.
-	// The values are property paths pointing to the property, like ["property"] (top-level) or ["container", "property"].
-	// The paths are in API-shape, that is, they contain flattened containers and the names are not SDK names.
-	ApiPathsToSubResourcesToMaintainIfUnset [][]string `json:"subResourcesToMaintainIfUnset,omitempty"`
 }
 
 type TypeLookupFunc func(ref string) (*AzureAPIType, bool, error)
@@ -185,7 +177,7 @@ func (res *AzureAPIResource) PathsToSubResourcePropertiesToMaintain(includeConta
 		typeLookup,
 		includeContainers,
 		func(propName string, prop AzureAPIProperty, path []string) {
-			if prop.maintainSubResourceIfUnset {
+			if prop.MaintainSubResourceIfUnset {
 				// make a copy of path since the original might be passed to other callbacks
 				pathToProperty := append([]string{}, path...)
 				pathToProperty = append(pathToProperty, propName)
