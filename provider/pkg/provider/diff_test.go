cdcd6c0af51309aaae026501738698ed7e77caf5
Thomas Kappler <tkappler@pulumi.com>
2024-01-04 11:52:01 +0100

    Extend keyed array diff test with azure id comparison

--- provider/pkg/provider/diff_test.go
+++ provider/pkg/provider/diff_test.go
@@ -661,10 +661,10 @@ func TestDiffKeyedArrays(t *testing.T) {
 		keys := []string{"p1"}
 
 		// The first object will be unchanged, the second updated, the third removed and replaced
-		// with a different one.
+		// with a different one. The unchanged one has case differences that shouldn't matter.
 		old := []resource.PropertyValue{
 			{V: resource.PropertyMap{
-				"p1": {V: "unchanged"},
+				"p1": {V: "/subscriptions/123/resourceGroups/rg/providers/Microsoft.Compute/something"},
 				"p2": {V: "v2"},
 				"p3": {V: "v3"},
 			}},
@@ -692,7 +692,7 @@ func TestDiffKeyedArrays(t *testing.T) {
 				"p2": {V: "v222"},
 			}},
 			{V: resource.PropertyMap{
-				"p1": {V: "unchanged"},
+				"p1": {V: "/subscriptions/123/resourcegroups/rg/providers/microsoft.compute/something"},
 				"p2": {V: "v2"},
 				"p3": {V: "v3"},
 			}},
@@ -709,8 +709,12 @@ func TestDiffKeyedArrays(t *testing.T) {
 		assert.Equal(t, 1, len(diff.Array.Adds))
 		assert.Equal(t, new[1], diff.Array.Adds[1])
 
-		assert.Equal(t, 1, len(diff.Array.Sames))
-		assert.Equal(t, old[0], diff.Array.Sames[2])
+		require.Equal(t, 1, len(diff.Array.Sames))
+		require.Contains(t, diff.Array.Sames, 2)
+		sameOld := old[0].ObjectValue()
+		sameNew := diff.Array.Sames[2].ObjectValue()
+		assert.Equal(t, normalizeAzureId(sameOld["p1"].StringValue()), normalizeAzureId(sameNew["p1"].StringValue()))
+		assert.Equal(t, sameOld["p2"], sameNew["p2"])
 
 		assert.Equal(t, 1, len(diff.Array.Deletes))
 		assert.Equal(t, old[2], diff.Array.Deletes[2])
