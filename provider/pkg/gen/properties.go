f65ea5bef29384bd93a2edb09b0a21beaadc5a20
Thomas Kappler <tkappler@pulumi.com>
2024-01-24 15:29:21 +0100

    Remove resolved special case for #331

--- provider/pkg/gen/properties.go
+++ provider/pkg/gen/properties.go
@@ -66,10 +66,7 @@ func (m *moduleGenerator) genProperties(resolvedSchema *openapi.Schema, isOutput
 			// a meaningful desired-state property.
 			continue
 		}
-		if name == "clusterID" && property.Description == "The deprecated identity" {
-			// TODO: Get rid of this in https://github.com/pulumi/pulumi-azure-native/issues/331
-			continue
-		}
+
 		// TODO: Workaround for https://github.com/pulumi/pulumi/issues/12629 - remove once merged
 		if (name == "conditionSets" && property.Items.Schema.Ref.String() == "#/definitions/GovernanceRuleConditionSets") ||
 			(name == "conditionSets" && property.Items.Schema.Ref.String() == "#/definitions/ApplicationConditionSets") {
