95abe576f6534b7f0c2608b6d11e05f299258fbd
Thomas Kappler <tkappler@pulumi.com>
2024-02-04 09:24:42 +0100

    Add test_examples CI job to test against p/examples test suite

---
+++ .github/scripts/test-in-workspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+test "${1}" = "" && echo "Usage: $0 <TestSet> (see pulumi/examples 'make specific_test_set')" && exit 1
+TestSet=${1}
+
+go work init
+go work use -r pulumi-azure-native/sdk/pulumi-azure-native-sdk
+go work use examples/misc/test
+
+cd examples
+make specific_test_set TestSet=$TestSet
