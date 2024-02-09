f318cfa65e10f5651acd095db82de37527f5c1d0
Thomas Kappler <tkappler@pulumi.com>
2024-02-09 17:13:22 +0100

    Simple example/test for legal hold

---
+++ examples/blobcontainer-legalhold/index.ts
@@ -0,0 +1,35 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("resourceGroup");
+
+// Create an Azure resource (Storage Account)
+const storageAccount = new storage.StorageAccount("sa", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
+
+const container = new storage.BlobContainer("container", {
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name,
+    containerName: "lhcontainer",
+    legalHold: {
+        tags: ["tag1"],
+    },
+});
+
+// Export the primary key of the Storage Account
+const storageAccountKeys = storage.listStorageAccountKeysOutput({
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name
+});
+export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
+export const containerName = container.name;
+export const accountName = storageAccount.name;
+export const resourceGroupName = resourceGroup.name;
