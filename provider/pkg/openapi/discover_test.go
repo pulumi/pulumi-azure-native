pulumi/pulumi-azure-native

commit c6113c2b1b488ce1999468253c192d345ee5865f

Skip delete for Sql TransparentDataEncryption
- Use nil to indicate we don't have a default state defined instead of HasDefault.
- Expose standalone SkipDeleteOperation function for the provider.
- Call into defaults function directly from the provider rather than serializing more properties into the metadata.
- We can't skip serializing the DefaultBody as there's some cases where it's implicitly set based on a `/list` path existing.
- Panic if we define duplicate default resource states (after normalization).

provider/pkg/openapi/discover.go
@@ -314,7 +314,7 @@ func addAPIPath(providers AzureProviders, fileLocation, path string, swagger *Sp
 		switch {
 		case pathItem.Get != nil && !pathItem.Get.Deprecated:
 			defaultState := defaults.GetDefaultResourceState(path)
-			if defaultState.HasDefault && hasDelete && defaultState.HasNonEmptyCollections {
+			if defaultState != nil && hasDelete && defaultState.HasNonEmptyCollections {
 				// See the limitation in `SdkShapeConverter.isDefaultResponse()`
 				panic(fmt.Sprintf("invalid defaultResourcesState '%s': non-empty collections aren't supported for deletable resources", path))
 			}
@@ -328,15 +328,19 @@ func addAPIPath(providers AzureProviders, fileLocation, path string, swagger *Sp
 				typeName = "PrivateRecordSet"
 			}
 
-			if typeName != "" && (hasDelete || defaultState.HasDefault) {
+			if typeName != "" && (hasDelete || defaultState != nil) {
 				if _, ok := version.Resources[typeName]; ok && version.Resources[typeName].Path != path {
 					fmt.Printf("warning: duplicate resource %s/%s at paths:\n  - %s\n  - %s\n", apiVersion, typeName, path, version.Resources[typeName].Path)
+				}
+				defaultBody := map[string]interface{}{}
+				if defaultState != nil {
+					defaultBody = defaultState.State
 				}
 				version.Resources[typeName] = &ResourceSpec{
 					Path:        path,
 					PathItem:    &pathItem,
 					Swagger:     swagger,
-					DefaultBody: defaultState.State,
+					DefaultBody: defaultBody,
 				}
 			}
 		case pathItem.Head != nil && !pathItem.Head.Deprecated:
@@ -361,7 +365,8 @@ func addAPIPath(providers AzureProviders, fileLocation, path string, swagger *Sp
 			}
 			if typeName != "" {
 				var defaultBody map[string]interface{}
-				if defaultState := defaults.GetDefaultResourceState(path); defaultState.HasDefault {
+				defaultState := defaults.GetDefaultResourceState(path)
+				if defaultState != nil {
 					defaultBody = defaultState.State
 				} else if !hasDelete {
 					// The /list pattern that we handle here seems to (almost) universally have this shape of the default body.
@@ -388,7 +393,7 @@ func addAPIPath(providers AzureProviders, fileLocation, path string, swagger *Sp
 	if pathItem.Patch != nil && !pathItem.Patch.Deprecated && pathItem.Get != nil && !pathItem.Get.Deprecated {
 		defaultState := defaults.GetDefaultResourceState(path)
 		typeName := resources.ResourceName(pathItem.Get.ID)
-		if typeName != "" && defaultState.HasDefault {
+		if typeName != "" && defaultState != nil {
 			if _, ok := version.Resources[typeName]; ok && version.Resources[typeName].Path != path {
 				fmt.Printf("warning: duplicate resource %s/%s at paths:\n  - %s\n  - %s\n", apiVersion, typeName, path, version.Resources[typeName].Path)
 			}

provider/pkg/openapi/defaults/defaultResourcesState.go
@@ -3,6 +3,7 @@
 package defaults
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/pulumi/pulumi-azure-native/provider/pkg/openapi/paths"
@@ -81,9 +82,6 @@ var defaultResourcesStateRaw = map[string]map[string]interface{}{
 	"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/securityAlertPolicies/{securityAlertPolicyName}": {
 		"state": "Disabled",
 	},
-	"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/transparentDataEncryption/{transparentDataEncryptionName}": {
-		"status": "Disabled",
-	},
 	"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/auditingSettings/{blobAuditingPolicyName}": {
 		"state": "Disabled",
 	},
@@ -154,17 +152,38 @@ var defaultResourcesStateRaw = map[string]map[string]interface{}{
 	},
 }
 
+var skipDeleteResources = map[string]bool{
+	// https://learn.microsoft.com/en-us/azure/azure-sql/database/transparent-data-encryption-tde-overview
+	"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/transparentDataEncryption/{tdeName}": true,
+}
+
 // defaultResourcesStateNormalized maps normalized paths of resources to default state of a resource. The default state is
 // the configuration of that resource after its parent resource is created and before any explicit PUT operations on
 // its endpoint. These resources are created automatically by Azure with their parents, therefore, the provider can
 // expect to see this state at the time of the Create operation.
-var defaultResourcesStateNormalized map[string]map[string]interface{}
+var defaultResourcesStateNormalized map[string]DefaultResourceState
 
 func init() {
-	defaultResourcesStateNormalized = map[string]map[string]interface{}{}
-	for key, value := range defaultResourcesStateRaw {
-		defaultResourcesStateNormalized[paths.NormalizePath(key)] = value
+	defaultResourcesStateNormalized = map[string]DefaultResourceState{}
+	for key := range skipDeleteResources {
+		addNormalisedState(key, DefaultResourceState{
+			SkipDelete: true,
+		})
 	}
+	for key, value := range defaultResourcesStateRaw {
+		addNormalisedState(key, DefaultResourceState{
+			State:                  value,
+			HasNonEmptyCollections: containsNonEmptyCollections(value),
+		})
+	}
+}
+
+func addNormalisedState(path string, state DefaultResourceState) {
+	normalizedPath := paths.NormalizePath(path)
+	if _, exists := defaultResourcesStateNormalized[normalizedPath]; exists {
+		panic(fmt.Errorf("FATAL: default state for %s is already set", normalizedPath))
+	}
+	defaultResourcesStateNormalized[normalizedPath] = state
 }
 
 func containsNonEmptyCollections(value map[string]interface{}) bool {
@@ -185,23 +204,27 @@ func containsNonEmptyCollections(value map[string]interface{}) bool {
 }
 
 type DefaultResourceState struct {
-	HasDefault             bool
-	State                  map[string]interface{}
+	// State is the default state of a resource.
+	State map[string]interface{}
+	// SkipDelete is true if the PUT operation should be skipped during the delete step.
+	SkipDelete bool
+	// HasNonEmptyCollections is true if any property of the default state is a non-empty collection.
 	HasNonEmptyCollections bool
 }
 
-func GetDefaultResourceState(path string) DefaultResourceState {
+func GetDefaultResourceState(path string) *DefaultResourceState {
 	normalizedPath := paths.NormalizePath(path)
-	if value, ok := defaultResourcesStateNormalized[normalizedPath]; ok {
-		return DefaultResourceState{
-			HasDefault:             true,
-			State:                  value,
-			HasNonEmptyCollections: containsNonEmptyCollections(value),
-		}
+	defaults, ok := defaultResourcesStateNormalized[normalizedPath]
+	if !ok {
+		return nil
 	}
-	return DefaultResourceState{
-		HasDefault:             false,
-		State:                  nil,
-		HasNonEmptyCollections: false,
+	return &defaults
+}
+
+func SkipDeleteOperation(path string) bool {
+	defaultState := GetDefaultResourceState(path)
+	if defaultState == nil {
+		return false
 	}
+	return defaultState.SkipDelete
 }

provider/pkg/openapi/defaults/defaultResourcesState_test.go
@@ -1,0 +1,50 @@
+package defaults
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test GetDefaultResourceState returns expected values for a given resource type.
+func TestNotFound(t *testing.T) {
+	actual := GetDefaultResourceState("notapath")
+	assert.Nil(t, actual)
+}
+
+func TestSkipDelete(t *testing.T) {
+	actual := GetDefaultResourceState("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/transparentDataEncryption/{tdeName}")
+	expected := DefaultResourceState{
+		SkipDelete: true,
+	}
+	assert.Equal(t, &expected, actual)
+}
+
+func TestPathNormalisation(t *testing.T) {
+	// Same as TestSkipDelete, but with a different casing and parameter naming.
+	actual := GetDefaultResourceState("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/transparentDataEncryption/{transparentDataEncryptionName}")
+	expected := DefaultResourceState{
+		SkipDelete: true,
+	}
+	assert.Equal(t, &expected, actual)
+}
+
+func TestEmptyBody(t *testing.T) {
+	actual := GetDefaultResourceState("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{accountName}/blobServices/{BlobServicesName}")
+	expected := DefaultResourceState{
+		State: map[string]interface{}{},
+	}
+	assert.Equal(t, &expected, actual)
+}
+
+func TestNonEmptyCollection(t *testing.T) {
+	actual := GetDefaultResourceState("/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Insights/components/{resourceName}/currentbillingfeatures")
+	expected := DefaultResourceState{
+		State: map[string]interface{}{
+			"currentBillingFeatures": []string{"Basic"},
+			"dataVolumeCap":          map[string]string{},
+		},
+		HasNonEmptyCollections: true,
+	}
+	assert.Equal(t, &expected, actual)
+}

provider/pkg/provider/provider.go
@@ -30,6 +30,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi-azure-native/provider/pkg/arm2pulumi"
+	"github.com/pulumi/pulumi-azure-native/provider/pkg/openapi/defaults"
 	"github.com/pulumi/pulumi-azure-native/provider/pkg/resources"
 	"github.com/pulumi/pulumi-azure-native/provider/pkg/version"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
@@ -1260,6 +1261,9 @@ func (k *azureNativeProvider) Delete(ctx context.Context, req *rpc.DeleteRequest
 	case res.Singleton:
 		// Singleton resources can't be deleted (or created), set them to the default state.
 		for _, param := range res.PutParameters {
+			if defaults.SkipDeleteOperation(res.Path) {
+				continue
+			}
 			if param.Location == "body" {
 				requestBody := k.converter.SdkPropertiesToRequestBody(param.Body.Properties, res.DefaultBody)
 

commit b05f9c4036c7eef07dfe403c374f843f1945826e

Distinguish nil from empty map as default state in resource discovery (#3076)
This PR fixes what I believe is a bug introduced in #2456. For the
default state of a resource, we need to distinguish between `nil` (no
default) and the empty object as the empty default. This is also
described in the doc comment of `AzureAPIResource.DefaultBody` in
resources.go:
> - omitempty is not set to distinct between nil (no default) and empty
maps (empty default)

`AzureClient.CanCreate` checks `DefaultBody != nil` to detect resources
that are automatically created with their parent.

The actual change in metadata is not visible in the PR because we don't
commit it. It looks like this:
```diff
--- ../m-master.json    2024-02-12 20:22:54.578408624 +0100
+++ ../m.json   2024-02-12 20:21:14.881957652 +0100
@@ -1005922,7 +1005922,7 @@
           ]
         }
       },
-      "defaultBody": {},
+      "defaultBody": null,
       "putAsyncStyle": "azure-async-operation",
       "deleteAsyncStyle": "azure-async-operation"
     },
```

provider/pkg/openapi/discover.go
@@ -558,7 +558,7 @@ func addResourcesAndInvokes(version VersionResources, fileLocation, path, provid
 				if _, ok := version.Resources[typeName]; ok && version.Resources[typeName].Path != path {
 					fmt.Printf("warning: duplicate resource %s/%s at paths:\n  - %s\n  - %s\n", apiVersion, typeName, path, version.Resources[typeName].Path)
 				}
-				defaultBody := map[string]interface{}{}
+				var defaultBody map[string]any
 				if defaultState != nil {
 					defaultBody = defaultState.State
 				}

provider/pkg/openapi/discover_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 
 	"github.com/go-openapi/spec"
+	"github.com/pulumi/pulumi-azure-native/v2/provider/pkg/openapi/defaults"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestListDiagnosticCategoriesShouldBeAdded(t *testing.T) {
@@ -13,7 +15,6 @@ func TestListDiagnosticCategoriesShouldBeAdded(t *testing.T) {
 	version := NewVersionResources()
 
 	swagger := Spec{
-
 		Swagger: spec.Swagger{
 			SwaggerProps: spec.SwaggerProps{
 				Info: &spec.Info{
@@ -46,4 +47,76 @@ func TestListDiagnosticCategoriesShouldBeAdded(t *testing.T) {
 	invoke, ok := version.Invokes["listDiagnosticSettingsCategory"]
 	assert.True(t, ok)
 	assert.Equal(t, path, invoke.Path)
+}
+
+func TestDefaultState(t *testing.T) {
+	makeSwagger := func(path string) Spec {
+		return Spec{
+			Swagger: spec.Swagger{
+				SwaggerProps: spec.SwaggerProps{
+					Info: &spec.Info{
+						InfoProps: spec.InfoProps{
+							Version: "2020-01-01",
+						},
+					},
+					Paths: &spec.Paths{
+						Paths: map[string]spec.PathItem{
+							path: {
+								// Needs GET, DELETE, PUT to be discovered as a resource
+								PathItemProps: spec.PathItemProps{
+									Get: &spec.Operation{
+										OperationProps: spec.OperationProps{
+											ID: "DiagnosticSettingsCategory_Get",
+										},
+									},
+									Put: &spec.Operation{
+										OperationProps: spec.OperationProps{
+											ID: "DiagnosticSettingsCategory_Put",
+										},
+									},
+									Delete: &spec.Operation{
+										OperationProps: spec.OperationProps{
+											ID: "DiagnosticSettingsCategory_Delete",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	parseSwagger := func(t *testing.T, path string) *ResourceSpec {
+		swagger := makeSwagger(path)
+		version := NewVersionResources()
+
+		addResourcesAndInvokes(version, "/file/path", path, "insights", &swagger)
+
+		require.NotEmpty(t, version.Resources)
+		res, ok := version.Resources["DiagnosticSettingsCategory"]
+		require.True(t, ok)
+		return res
+	}
+
+	t.Run("No default", func(t *testing.T) {
+		path := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Insights/diagnosticSettingsCategories/{categoryName}"
+
+		def := defaults.GetDefaultResourceState(path)
+		require.Nil(t, def)
+
+		res := parseSwagger(t, path)
+		assert.Nil(t, res.DefaultBody)
+	})
+
+	t.Run("With default", func(t *testing.T) {
+		path := "/{resourceId}/providers/Microsoft.Security/advancedThreatProtectionSettings/{settingName}"
+
+		def := defaults.GetDefaultResourceState(path)
+		require.NotNil(t, def)
+
+		res := parseSwagger(t, path)
+		assert.Equal(t, def.State, res.DefaultBody)
+	})
 }