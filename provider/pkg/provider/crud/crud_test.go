3e918dcd83242d7c2f3658a6fd6d8760cd0dfa59
Thomas Kappler <tkappler@pulumi.com>
2024-03-12 15:43:06 +0100

    Show a reasonable error when a path parameter is not a string

---
+++ provider/pkg/provider/crud/crud_test.go
@@ -0,0 +1,45 @@
+package crud
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi-azure-native/v2/provider/pkg/resources"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPathParamsErrorHandling(t *testing.T) {
+	t.Run("No params, no error", func(t *testing.T) {
+		_, _, err := PrepareAzureRESTIdAndQuery("/path", []resources.AzureAPIParameter{}, nil, nil)
+		assert.NoError(t, err)
+	})
+
+	t.Run("String params, no error", func(t *testing.T) {
+		_, _, err := PrepareAzureRESTIdAndQuery("/path",
+			[]resources.AzureAPIParameter{
+				{
+					Name:     "p1",
+					Location: "path",
+					Value:    &resources.AzureAPIProperty{Type: "string"},
+				},
+			}, map[string]any{
+				"p1": "yay",
+			}, nil)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Non-string params, error", func(t *testing.T) {
+		_, _, err := PrepareAzureRESTIdAndQuery("/path",
+			[]resources.AzureAPIParameter{
+				{
+					Name:     "p1",
+					Location: "path",
+					Value:    &resources.AzureAPIProperty{Type: "string"}, // correct, but value is not
+				},
+			}, map[string]any{
+				"p1": 42,
+			}, nil)
+		if assert.Error(t, err) {
+			assert.Equal(t, "expected string value for path parameter 'p1', got int", err.Error())
+		}
+	})
+}
