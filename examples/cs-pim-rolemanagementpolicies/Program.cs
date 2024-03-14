85b11b44d9934d6ec2f697330ac1397c98e96783
Thomas Kappler <tkappler@pulumi.com>
2024-03-14 16:57:20 +0100

    Simple example. Doesn't work yet due to nested rules array

---
+++ examples/cs-pim-rolemanagementpolicies/Program.cs
@@ -0,0 +1,40 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Authorization;
+using Pulumi.AzureNative.Authorization.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(async () =>
+{
+    var clientConfig = await GetClientConfig.InvokeAsync();
+
+    var policy = new RoleManagementPolicy("policy", new RoleManagementPolicyArgs
+    {
+        RoleManagementPolicyName = "3faafb81-7f6f-4c66-b936-fb41ef4e4734",
+        Scope =  Output.Format($"subscriptions/{clientConfig.SubscriptionId}"),
+        Rules = new InputList<ResourceArgs>
+        {
+            new RoleManagementPolicyExpirationRuleArgs
+            {
+                Id = "Expiration_Admin_Eligibility",
+                IsExpirationRequired = true,
+                MaximumDuration = "P90D",
+                RuleType = "RoleManagementPolicyExpirationRule",
+            },
+            new RoleManagementPolicyNotificationRuleArgs
+            {
+                Id = "Notification_Admin_Admin_Eligibility",
+                IsDefaultRecipientsEnabled = true,
+                NotificationLevel = "All",
+                NotificationType = "Email",
+                RecipientType = "Admin",
+                RuleType = "RoleManagementPolicyNotificationRule",
+            },
+        },
+    });
+
+    // Export the primary key of the Storage Account
+    return new Dictionary<string, object?>
+    {
+        {"policy", policy.Name}
+    };
+});
