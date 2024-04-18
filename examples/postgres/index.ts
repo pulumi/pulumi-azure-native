c924efdd91dfa67e54fc1ddf54f8d1e143b954f4
Thomas Kappler <tkappler@pulumi.com>
2024-04-18 10:40:25 +0200

    Update and reactivate postgres test (#3218)
    I've manually run the nodejs tests twice in addition to the regular CI run.
    
    - Update the postgres test so it builds. Limit to the current Flexible
    Server API., Single Server is long deprecated by Azure.
    - Re-enable the test.
    
    Resolves #898

--- examples/postgres/index.ts
+++ examples/postgres/index.ts
@@ -1,41 +1,11 @@
 // Copyright 2023, Pulumi Corporation.  All rights reserved.
 
 import * as postgresql from "@pulumi/azure-native/dbforpostgresql";
-import * as postgresqlflexible from "@pulumi/azure-native/dbforpostgresql/v20221201";
 import * as resources from "@pulumi/azure-native/resources";
 
 const resourceGroup = new resources.ResourceGroup("rg");
 
-const server = new postgresql.Server("server", {
-    resourceGroupName: resourceGroup.name,
-    location: resourceGroup.location,
-    properties: {
-        administratorLogin: "cloudsa",
-        administratorLoginPassword: `pa$$w0rd`,
-        createMode: "Default",
-        minimalTlsVersion: postgresql.MinimalTlsVersionEnum.TLS1_2,
-        sslEnforcement: postgresql.SslEnforcementEnum.Enabled,
-        storageProfile: {
-            backupRetentionDays: 7,
-            geoRedundantBackup: postgresql.GeoRedundantBackup.Disabled,
-            storageMB: 128000,
-        },
-    },
-    sku: {
-        capacity: 2,
-        family: "Gen5",
-        name: "B_Gen5_2",
-        tier: postgresql.SkuTier.Basic,
-    },
-});
-
-new postgresql.Configuration("backslash_quote", {
-    resourceGroupName: resourceGroup.name,
-    serverName: server.name,
-    value: "on",
-});
-
-const flexibleServer = new postgresqlflexible.Server("server", {
+const flexibleServer = new postgresql.Server("server", {
     resourceGroupName: resourceGroup.name,
     sku: {
         tier: "GeneralPurpose",
@@ -49,7 +19,7 @@ const flexibleServer = new postgresqlflexible.Server("server", {
     },
 });
 
-new postgresqlflexible.Configuration("backslash_quote", {
+new postgresql.Configuration("backslash_quote", {
     resourceGroupName: resourceGroup.name,
     serverName: flexibleServer.name,
     source: "user-override",
