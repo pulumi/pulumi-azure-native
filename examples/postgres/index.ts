c1bcc4c09986ad1032019aa35cb5356d43ea6f54
Thomas Kappler <tkappler@pulumi.com>
2024-04-19 12:10:10 +0200

    Re-disable Postgres test (#3227)
    #3218 was a bust; I might have messed something up when testing it.
    Re-opened #898 as well. At least the test code is updated to v2.

--- examples/postgres/index.ts
+++ examples/postgres/index.ts
@@ -3,13 +3,21 @@
 import * as postgresql from "@pulumi/azure-native/dbforpostgresql";
 import * as resources from "@pulumi/azure-native/resources";
 
-const resourceGroup = new resources.ResourceGroup("rg");
+const resourceGroup = new resources.ResourceGroup("rg", {
+    // westus2 was not available for Postgres Flexible Server at the time of writing, see
+    // https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/overview#azure-regions
+    location: "uksouth",
+});
 
 const flexibleServer = new postgresql.Server("server", {
+    location: resourceGroup.location,
     resourceGroupName: resourceGroup.name,
     sku: {
         tier: "GeneralPurpose",
         name: "Standard_D2s_v3",
+    },
+    highAvailability: {
+        mode: postgresql.HighAvailabilityMode.Disabled,
     },
     administratorLogin: "cloudsa",
     administratorLoginPassword: `pa$$w0rd`,
