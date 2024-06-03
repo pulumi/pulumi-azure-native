2206dbc20335a3bf09c43eda9b07a2f3ea491108
Thomas Kappler <tkappler@pulumi.com>
2024-06-03 17:16:06 +0200

    Simplify Delete logic for singletons take 2

--- provider/pkg/provider/provider.go
+++ provider/pkg/provider/provider.go
@@ -1385,24 +1385,19 @@ func (k *azureNativeProvider) Delete(ctx context.Context, req *rpc.DeleteRequest
 		}
 	case res.Singleton:
 		// Singleton resources can't be deleted (or created), set them to the default state.
-		for _, param := range res.PutParameters {
-			if defaults.SkipDeleteOperation(res.Path) {
-				continue
+		if body, ok := res.BodyParameter(); ok && !defaults.SkipDeleteOperation(res.Path) {
+			requestBody := k.converter.SdkInputsToRequestBody(body.Body.Properties, res.DefaultBody, id)
+
+			queryParams := map[string]interface{}{"api-version": res.APIVersion}
+
+			// Submit the `PUT` or `PATCH` against the ARM endpoint
+			op := k.azureClient.Put
+			if res.UpdateMethod == "PATCH" {
+				op = k.azureClient.Patch
 			}
-			if param.Location == "body" {
-				requestBody := k.converter.SdkInputsToRequestBody(param.Body.Properties, res.DefaultBody, id)
-
-				queryParams := map[string]interface{}{"api-version": res.APIVersion}
-
-				// Submit the `PUT` or `PATCH` against the ARM endpoint
-				op := k.azureClient.Put
-				if res.UpdateMethod == "PATCH" {
-					op = k.azureClient.Patch
-				}
-				_, _, err := op(ctx, id, requestBody, queryParams, res.PutAsyncStyle)
-				if err != nil {
-					return nil, azure.AzureError(err)
-				}
+			_, _, err := op(ctx, id, requestBody, queryParams, res.PutAsyncStyle)
+			if err != nil {
+				return nil, azure.AzureError(err)
 			}
 		}
 	default:
