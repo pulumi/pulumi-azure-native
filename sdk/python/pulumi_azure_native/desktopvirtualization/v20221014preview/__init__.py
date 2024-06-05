e20b0e87d6579e5d5e83ebc9f2051aa6b222c7ae
Thomas Kappler <tkappler@pulumi.com>
2024-06-05 10:20:29 +0200

    regenerate

--- sdk/python/pulumi_azure_native/desktopvirtualization/v20221014preview/__init__.py
+++ sdk/python/pulumi_azure_native/desktopvirtualization/v20221014preview/__init__.py
@@ -11,6 +11,7 @@ from .application_group import *
 from .get_application import *
 from .get_application_group import *
 from .get_host_pool import *
+from .get_host_pool_registration_token import *
 from .get_msix_package import *
 from .get_private_endpoint_connection_by_host_pool import *
 from .get_private_endpoint_connection_by_workspace import *
