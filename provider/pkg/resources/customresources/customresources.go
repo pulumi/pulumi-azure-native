e9c8fc6146c8afe14504db24c0db2d7fdfa31412
Thomas Kappler <tkappler@pulumi.com>
2024-10-07 17:58:02 +0200

    Revert "WIP custom static website, wired up and compiles but untested"
    To be continued later.
    
    This reverts commit b798d6993c684475a7e8de7caf79e302a26a46ca.

--- provider/pkg/resources/customresources/customresources.go
+++ provider/pkg/resources/customresources/customresources.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/keyvault/armkeyvault"
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
 	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2019-04-01/storage"
@@ -152,7 +151,6 @@ func (r *CustomResource) ApplySchema(pkg *schema.PackageSpec, meta *AzureAPIMeta
 
 // BuildCustomResources creates a map of custom resources for given environment parameters.
 func BuildCustomResources(env *azureEnv.Environment,
-	cloud *cloud.Configuration,
 	azureClient azure.AzureClient,
 	lookupResource ResourceLookupFunc,
 	crudClientFactory crud.ResourceCrudClientFactory,
@@ -185,15 +183,13 @@ func BuildCustomResources(env *azureEnv.Environment,
 		return nil, err
 	}
 
-	// TODO,tkappler PULUMI_USE_AUTOREST or similar for auth
 	resources := []*CustomResource{
 		// Azure KeyVault resources.
 		keyVaultSecret(env.KeyVaultDNSSuffix, &kvClient),
 		keyVaultKey(env.KeyVaultDNSSuffix, &kvClient),
 		keyVaultAccessPolicy(armKVClient),
 		// Storage resources.
-		// newStorageAccountStaticWebsite(env, &storageAccountsClient),
-		newStorageAccountStaticWebsite_azidentity(cloud, tokenCred),
+		newStorageAccountStaticWebsite(env, &storageAccountsClient),
 		newBlob(env, &storageAccountsClient),
 		// Customization of regular resources
 		blobContainerLegalHold(azureClient),
@@ -210,7 +206,7 @@ func BuildCustomResources(env *azureEnv.Environment,
 }
 
 // featureLookup is a map of custom resource to lookup their capabilities.
-var featureLookup, _ = BuildCustomResources(&azureEnv.Environment{}, nil, nil, nil, nil, "", nil, nil, nil, "", nil)
+var featureLookup, _ = BuildCustomResources(&azureEnv.Environment{}, nil, nil, nil, "", nil, nil, nil, "", nil)
 
 // HasCustomDelete returns true if a custom DELETE operation is defined for a given API path.
 func HasCustomDelete(path string) bool {
