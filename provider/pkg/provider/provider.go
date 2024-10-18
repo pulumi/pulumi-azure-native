d8ef7d713909c921256e590b548000d8ed2829ff
Daniel Bradley <daniel@pulumi.com>
2024-10-18 13:50:30 +0100

    Default to azcore enabled

--- provider/pkg/provider/provider.go
+++ provider/pkg/provider/provider.go
@@ -1612,3 +1612,17 @@ func (k *azureNativeProvider) autorestEnvToHamiltonEnv() environments.Environmen
 		return environments.Global
 	}
 }
+
+// enableAzcoreBackend is a feature toggle that returns true if the newer backend using azcore and
+// azidentity for REST and authentication should be used. Otherwise, the previous autorest backend
+// is used.
+// Tracked in epic #3576, the new backend was added to upgrade from unmaintained libraries that
+// don't receive security and other updates. It uses the latest official Azure packages.
+// The new backend is gated behind this feature toggle to allow enabling it selectively,
+// limiting the blast radius of regressions. It's enabled in the daily CI workflow azcore-scheduled.
+func enableAzcoreBackend() bool {
+	if enable, ok := os.LookupEnv("PULUMI_ENABLE_AZCORE_BACKEND"); ok {
+		return enable == "true"
+	}
+	return true
+}
