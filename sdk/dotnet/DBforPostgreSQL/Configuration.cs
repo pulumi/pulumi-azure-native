2378d8215ac18be4af6ce84eca8078027ae72df9
Thomas Kappler <tkappler@pulumi.com>
2024-12-05 16:22:08 +0100

    regenerate

--- sdk/dotnet/DBforPostgreSQL/Configuration.cs
+++ sdk/dotnet/DBforPostgreSQL/Configuration.cs
@@ -127,9 +127,6 @@ namespace Pulumi.AzureNative.DBforPostgreSQL
                 Version = Utilities.Version,
                 Aliases =
                 {
-                    new global::Pulumi.Alias { Type = "azure-native:dbforpostgresql/v20200214preview:Configuration" },
-                    new global::Pulumi.Alias { Type = "azure-native:dbforpostgresql/v20200214privatepreview:Configuration" },
-                    new global::Pulumi.Alias { Type = "azure-native:dbforpostgresql/v20210410privatepreview:Configuration" },
                     new global::Pulumi.Alias { Type = "azure-native:dbforpostgresql/v20210601:Configuration" },
                     new global::Pulumi.Alias { Type = "azure-native:dbforpostgresql/v20210601preview:Configuration" },
                     new global::Pulumi.Alias { Type = "azure-native:dbforpostgresql/v20210615privatepreview:Configuration" },
