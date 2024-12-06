ab1ed99e83f1d02b05581d92e687de7c19ce21c3
Daniel Bradley <daniel@pulumi.com>
2024-12-06 11:22:59 +0000

    Add DefaultVersionLock.IsAtVersion
    - Make DefaultVersionLock a type definition instead of an alias.
    - Add IsAtVersion method.
    - Refactor usages in ShouldInclude.

--- provider/pkg/versioning/gen.go
+++ provider/pkg/versioning/gen.go
@@ -41,22 +41,12 @@ func (v VersionMetadata) ShouldInclude(provider string, version *openapi.ApiVers
 		return true
 	}
 	// Keep any resources in the default version lock
-	if resources, ok := v.Lock[provider]; ok {
-		if defaultResourceVersion, ok := resources[typeName]; ok {
-			if defaultResourceVersion == *version {
-				return true
-			}
-		}
+	if v.Lock.IsAtVersion(provider, typeName, *version) {
+		return true
 	}
 	// Keep any resources in the previous version lock for easier migration
-	if v.MajorVersion >= 3 {
-		if resources, ok := v.PreviousLock[provider]; ok {
-			if previousDefaultResourceVersion, ok := resources[typeName]; ok {
-				if previousDefaultResourceVersion == *version {
-					return true
-				}
-			}
-		}
+	if v.MajorVersion >= 3 && v.PreviousLock.IsAtVersion(provider, typeName, *version) {
+		return true
 	}
 	// Exclude versions from removed versions
 	if versions, ok := v.RemovedVersions[provider]; ok {
