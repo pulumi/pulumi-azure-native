a5fbb4533e70de8b7da0a98c4f2b2c7652bd6e0a
Daniel Bradley <daniel@pulumi.com>
2024-12-09 15:27:27 +0000

    Add virtual network & vm upgrade test

--- provider/pkg/provider/provider_e2e_test.go
+++ provider/pkg/provider/provider_e2e_test.go
@@ -80,6 +80,11 @@ func TestUpgradeKeyVault_2_76_0(t *testing.T) {
 	upgradeTest(t, "upgrade-keyvault", "2.76.0")
 }
 
+func TestUpgradeNetworkedVm_2_76_0(t *testing.T) {
+	t.Parallel()
+	upgradeTest(t, "upgrade-networked-vm", "2.76.0")
+}
+
 func upgradeTest(t *testing.T, testProgramDir string, upgradeFromVersion string, opts ...optproviderupgrade.PreviewProviderUpgradeOpt) {
 	t.Helper()
 	if testing.Short() {
