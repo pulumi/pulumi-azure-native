0c144a8c183c1b0b10834826b25bcb0ec5e91a54
Eron Wright <eron@pulumi.com>
2024-12-10 11:37:40 -0800

    [v34] Remove __inputs in state

--- provider/pkg/version/version.go
+++ provider/pkg/version/version.go
@@ -2,5 +2,11 @@
 
 package version
 
+import "github.com/blang/semver"
+
 // Version is initialized by the Go linker to contain the semver of this build.
 var Version string
+
+func GetVersion() semver.Version {
+	return semver.MustParse(Version)
+}
