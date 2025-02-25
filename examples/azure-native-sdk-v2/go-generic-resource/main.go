6889f75a1a230ebb1acf3d3ed240bb777ecd360b
Thomas Kappler <tkappler@pulumi.com>
2025-02-25 15:30:19 +0100

    integration test

---
+++ examples/azure-native-sdk-v2/go-generic-resource/main.go
@@ -0,0 +1,35 @@
+// Copyright 2025, Pulumi Corporation.  All rights reserved.
+
+package main
+
+import (
+	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an Azure Resource Group
+		resourceGroup, err := resources.NewResourceGroup(ctx, "resourceGroup", nil)
+		if err != nil {
+			return err
+		}
+
+		_, err = resources.NewResource(ctx, "acc", &resources.ResourceArgs{
+			ResourceProviderNamespace: pulumi.String("Microsoft.Storage"),
+			ResourceType:              pulumi.String("storageAccounts"),
+			ApiVersion:                pulumi.String("2022-09-01"),
+			ParentResourcePath:        pulumi.String(""),
+			ResourceGroupName:         resourceGroup.Name,
+			Kind:                      pulumi.String("StorageV2"),
+			Sku: &resources.SkuArgs{
+				Name: pulumi.String("Standard_LRS"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
