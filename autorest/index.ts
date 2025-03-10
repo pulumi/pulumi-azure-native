dd49f596586866207c12e52e756d76415427e164
Daniel Bradley <daniel@pulumi.com>
2025-03-10 10:00:09 +0000

    Join autorest input files to module resources
    - Fix paths relative root to match existing reports.
    - Generate diff against current v3 selection (from branch).

--- autorest/index.ts
+++ autorest/index.ts
@@ -1,5 +1,5 @@
 import { glob } from "fast-glob";
-import { writeFile } from "fs/promises";
+import { writeFile, readFile } from "fs/promises";
 import * as path from "path";
 import { fileURLToPath } from "url";
 import { ConfigurationLoader } from "@autorest/configuration";
@@ -9,6 +9,7 @@ import {
   AutorestSyncLogger,
 } from "@autorest/common";
 import { createFolderUri, createFileOrFolderUri } from "@azure-tools/uri";
+import yaml from "yaml";
 
 (async () => {
   const logger = new AutorestSyncLogger({
@@ -21,12 +22,8 @@ import { createFolderUri, createFileOrFolderUri } from "@azure-tools/uri";
     )
   );
 
-  const specificationRootDir = path.resolve(
-    "..",
-    "azure-rest-api-specs",
-    "specification"
-  );
-  const readmeFiles = await glob("*/resource-manager/readme.md", {
+  const specificationRootDir = path.resolve("..", "azure-rest-api-specs");
+  const readmeFiles = await glob("specification/*/resource-manager/readme.md", {
     cwd: specificationRootDir,
   });
 
@@ -50,5 +47,43 @@ import { createFolderUri, createFileOrFolderUri } from "@azure-tools/uri";
     "../reports/autorest-input-files.json",
     JSON.stringify(result, null, 2)
   );
-  console.log(`found ${result.length} input files`);
+
+  const autorestSpecFilePathsLookup = new Set<string>(result);
+
+  const allResourcesByVersionContent = await readFile(
+    "../reports/allResourcesByVersion.json",
+    "utf-8"
+  );
+  const allResourcesByVersion: Record<
+    ProviderModule,
+    Record<ApiVersion, Record<ResourceName, VersionEntry>>
+  > = JSON.parse(allResourcesByVersionContent);
+  const specVersions: Record<
+    ProviderModule,
+    Record<ResourceName, VersionEntry>
+  > = {};
+  for (const [providerModule, versions] of Object.entries(
+    allResourcesByVersion
+  )) {
+    const module: Record<ResourceName, VersionEntry> = {};
+    specVersions[providerModule] = module;
+    for (const resources of Object.values(versions)) {
+      for (const [resourceName, entry] of Object.entries(resources)) {
+        if (autorestSpecFilePathsLookup.has(entry.SpecFilePath)) {
+          module[resourceName] = entry;
+        }
+      }
+    }
+  }
+  writeFile("../reports/autorest.yaml", yaml.stringify(specVersions, null, 2));
 })().catch(console.error);
+
+type ProviderModule = string;
+type ApiVersion = string;
+type ResourceName = string;
+type VersionEntry = {
+  ApiVersion: ApiVersion;
+  SpecFilePath: string;
+  ResourceUri: string;
+  RpNamespace: string;
+};
