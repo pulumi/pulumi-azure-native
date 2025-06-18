8b775e87904fc41bf841adf9d463cc0813ce3b4a
Eron Wright <eron@pulumi.com>
2025-06-18 11:47:14 -0700

    remove getAuthConfig

--- provider/pkg/provider/auth.go
+++ provider/pkg/provider/auth.go
@@ -9,169 +9,8 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
-	"github.com/Azure/go-autorest/autorest"
-	azureEnv "github.com/Azure/go-autorest/autorest/azure"
-	"github.com/hashicorp/go-azure-helpers/authentication"
-	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi-azure-native/v2/provider/pkg/azure/cloud"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
-
 	goversion "github.com/hashicorp/go-version"
 )
-
-type authConfig struct {
-	*authentication.Config
-	useCli bool
-}
-
-func (a *authConfig) autorestEnvironment() (azureEnv.Environment, error) {
-	envName := a.Environment
-	env, err := azureEnv.EnvironmentFromName(envName)
-	if err != nil {
-		env, err = azureEnv.EnvironmentFromName(fmt.Sprintf("AZURE%sCLOUD", envName))
-		if err != nil {
-			return azureEnv.Environment{}, errors.Wrapf(err, "environment %q was not found", envName)
-		}
-	}
-	return env, nil
-}
-
-func (a *authConfig) cloud() cloud.Configuration {
-	cloudName := "public"
-	if a.Config != nil && a.Config.Environment != "" {
-		cloudName = a.Config.Environment
-	}
-	wellknown, _ := cloud.FromName(cloudName) // defaults to public
-	return wellknown
-}
-
-type oidcConfig struct {
-	oidcToken         string
-	oidcTokenFilePath string
-	oidcRequestToken  string
-	oidcRequestUrl    string
-}
-
-// Assumes that OIDC authentication is requested. Accordingly, returns an error if OIDC is not
-// properly configured. If ARM_OIDC_TOKEN/azure-native:oidcToken is set, it is preferred.
-func (k *azureNativeProvider) determineOidcConfig() (oidcConfig, error) {
-	oidcToken := k.getConfig("oidcToken", "ARM_OIDC_TOKEN")
-	if oidcToken != "" {
-		return oidcConfig{
-			oidcToken: oidcToken,
-		}, nil
-	}
-
-	oidcTokenFilePath := k.getConfig("oidcTokenFilePath", "ARM_OIDC_TOKEN_FILE_PATH")
-	if oidcTokenFilePath != "" {
-		return oidcConfig{
-			oidcTokenFilePath: oidcTokenFilePath,
-		}, nil
-	}
-
-	oidcRequestToken := k.getConfig("oidcRequestToken", "ARM_OIDC_REQUEST_TOKEN")
-	oidcRequestUrl := k.getConfig("oidcRequestUrl", "ARM_OIDC_REQUEST_URL")
-	if oidcRequestToken != "" && oidcRequestUrl != "" {
-		return oidcConfig{
-			oidcRequestToken: oidcRequestToken,
-			oidcRequestUrl:   oidcRequestUrl,
-		}, nil
-	}
-
-	// GitHub Actions pre-set env
-	oidcRequestToken = k.getConfig("oidcRequestToken", "ACTIONS_ID_TOKEN_REQUEST_TOKEN")
-	oidcRequestUrl = k.getConfig("oidcRequestUrl", "ACTIONS_ID_TOKEN_REQUEST_URL")
-	if oidcRequestToken != "" && oidcRequestUrl != "" {
-		return oidcConfig{
-			oidcRequestToken: oidcRequestToken,
-			oidcRequestUrl:   oidcRequestUrl,
-		}, nil
-	}
-
-	return oidcConfig{}, fmt.Errorf(`OIDC authentication was requested via useOidc/ARM_USE_OIDC but no token or request URL were 
-configured. See https://www.pulumi.com/registry/packages/azure-native/installation-configuration/#credentials for more information.`)
-}
-
-func (k *azureNativeProvider) getAuthConfig() (*authConfig, error) {
-	auxTenantsString := k.getConfig("auxiliaryTenantIds", "ARM_AUXILIARY_TENANT_IDS")
-	var auxTenants []string
-	if auxTenantsString != "" {
-		err := json.Unmarshal([]byte(auxTenantsString), &auxTenants)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to unmarshal '%s' as Auxiliary Tenants", auxTenantsString)
-		}
-	}
-	envName := readAzureEnvironmentFromConfig(k)
-
-	useOIDC := k.getConfig("useOidc", "ARM_USE_OIDC") == "true"
-	oidcConf, err := k.determineOidcConfig()
-	if useOIDC && err != nil {
-		return nil, err
-	}
-
-	builder := &authentication.Builder{
-		SubscriptionID:       k.getConfig("subscriptionId", "ARM_SUBSCRIPTION_ID"),
-		ClientID:             k.getConfig("clientId", "ARM_CLIENT_ID"),
-		TenantID:             k.getConfig("tenantId", "ARM_TENANT_ID"),
-		Environment:          envName,
-		MsiEndpoint:          k.getConfig("msiEndpoint", "ARM_MSI_ENDPOINT"),
-		AuxiliaryTenantIDs:   auxTenants,
-		ClientSecretDocsLink: "https://www.pulumi.com/docs/intro/cloud-providers/azure/setup/#service-principal-authentication",
-		MetadataHost:         k.getConfig("metadataHost", "ARM_METADATA_HOSTNAME"),
-
-		// Service Principal section.
-		ClientCertPath:     k.getConfig("clientCertificatePath", "ARM_CLIENT_CERTIFICATE_PATH"),
-		ClientCertPassword: k.getConfig("clientCertificatePassword", "ARM_CLIENT_CERTIFICATE_PASSWORD"),
-		ClientSecret:       k.getConfig("clientSecret", "ARM_CLIENT_SECRET"),
-
-		// OIDC section.
-		IDTokenRequestToken: oidcConf.oidcRequestToken,
-		IDTokenRequestURL:   oidcConf.oidcRequestUrl,
-		IDToken:             oidcConf.oidcToken,
-		IDTokenFilePath:     oidcConf.oidcTokenFilePath,
-
-		// Feature Toggles
-		SupportsClientCertAuth:         true,
-		SupportsClientSecretAuth:       true,
-		SupportsOIDCAuth:               useOIDC,
-		UseMicrosoftGraph:              useOIDC,
-		SupportsManagedServiceIdentity: k.getConfig("useMsi", "ARM_USE_MSI") == "true",
-		SupportsAzureCliToken:          !useOIDC,
-		SupportsAuxiliaryTenants:       len(auxTenants) > 0,
-	}
-
-	needCli := needAzCli(builder)
-	if needCli {
-		requireCliHint := "MSI, OIDC, client secret and client certificate authentication are not configured so we require the AZ CLI for authentication"
-		// Check that we have a good version of the cli (#1565). The check needs to happen before
-		// builder.Build(), or we return the less fitting error message from go-azure-helpers.
-		v, err := getAzVersion()
-		if err != nil {
-			return nil, fmt.Errorf("checking az cli version: %w: %s", err, requireCliHint)
-		}
-		if err = assertAzVersion(v); err != nil {
-			return nil, err
-		}
-	}
-
-	c, err := builder.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	// CLI authentication can only use the environment that the CLI is configured for.
-	// Fail early if that's not the one from config, to avoid obscure errors about missing subscriptions or endpoints later.
-	if c.Environment != envName {
-		return nil, errors.Errorf(`The configured Azure environment '%s' does not match the determined environment '%s'.
-When authenticating using the Azure CLI, the configured environment needs to match the one shown by 'az account show'.
-You can change environments using 'az cloud set --name <environment>'.`,
-			envName, c.Environment)
-	}
-
-	return &authConfig{c, needCli}, nil
-}
 
 func readAzureEnvironmentFromConfig(k *azureNativeProvider) string {
 	envName := k.getConfig("environment", "ARM_ENVIRONMENT")
@@ -182,14 +21,6 @@ func readAzureEnvironmentFromConfig(k *azureNativeProvider) string {
 		envName = "public"
 	}
 	return envName
-}
-
-// Without any of the other methods above, we fall back to the `az` CLI to authenticate.
-func needAzCli(builder *authentication.Builder) bool {
-	return !builder.SupportsManagedServiceIdentity &&
-		!builder.SupportsOIDCAuth &&
-		builder.ClientSecret == "" &&
-		builder.ClientCertPath == ""
 }
 
 func getAzVersion() (*goversion.Version, error) {
@@ -215,9 +46,9 @@ func getAzVersion() (*goversion.Version, error) {
 }
 
 func assertAzVersion(version *goversion.Version) error {
-	const versionHint = `Please make sure that the Azure CLI is installed in a version of at least 2.37 but less than 
-3.x; or configure another authentication method. See 
-https://www.pulumi.com/registry/packages/azure-native/installation-configuration/#credentials 
+	const versionHint = `Please make sure that the Azure CLI is installed in a version of at least 2.37 but less than
+3.x; or configure another authentication method. See
+https://www.pulumi.com/registry/packages/azure-native/installation-configuration/#credentials
 for more information.`
 
 	// We need this version because it doesn't print the error of #1565
--- provider/pkg/provider/auth_test.go
+++ provider/pkg/provider/auth_test.go
@@ -6,9 +6,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/go-azure-helpers/authentication"
+	azcloud "github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	goversion "github.com/hashicorp/go-version"
-	"github.com/pulumi/pulumi-azure-native/v2/provider/pkg/azure/cloud"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -35,142 +34,7 @@ func TestAcceptedAzVersions(t *testing.T) {
 	}
 }
 
-func TestOidcConfigFromGithub(t *testing.T) {
-	p := azureNativeProvider{}
-
-	// GH pre-set env
-	t.Setenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN", "t1")
-	t.Setenv("ACTIONS_ID_TOKEN_REQUEST_URL", "url1")
-
-	conf, err := p.determineOidcConfig()
-	assert.Nil(t, err)
-	assert.Equal(t, "t1", conf.oidcRequestToken)
-	assert.Equal(t, "url1", conf.oidcRequestUrl)
-	assert.Empty(t, conf.oidcToken)
-}
-
-func TestOidcConfigWithRequestUrlFromEnv(t *testing.T) {
-	p := azureNativeProvider{}
-
-	t.Setenv("ARM_OIDC_REQUEST_TOKEN", "t1")
-	t.Setenv("ARM_OIDC_REQUEST_URL", "url1")
-
-	conf, err := p.determineOidcConfig()
-	assert.Nil(t, err)
-	assert.Equal(t, "t1", conf.oidcRequestToken)
-	assert.Equal(t, "url1", conf.oidcRequestUrl)
-	assert.Empty(t, conf.oidcToken)
-}
-
-func TestOidcConfigWithRequestUrlFromConfig(t *testing.T) {
-	p := azureNativeProvider{
-		config: map[string]string{
-			"oidcRequestToken": "t1",
-			"oidcRequestUrl":   "url1",
-		},
-	}
-
-	conf, err := p.determineOidcConfig()
-	assert.Nil(t, err)
-	assert.Equal(t, "t1", conf.oidcRequestToken)
-	assert.Equal(t, "url1", conf.oidcRequestUrl)
-	assert.Empty(t, conf.oidcToken)
-}
-
-func TestOidcConfigWithTokenFromEnv(t *testing.T) {
-	p := azureNativeProvider{}
-
-	t.Setenv("ARM_OIDC_TOKEN", "t2")
-
-	conf, err := p.determineOidcConfig()
-	assert.Nil(t, err)
-	assert.Empty(t, conf.oidcRequestToken)
-	assert.Empty(t, conf.oidcRequestUrl)
-	assert.Equal(t, "t2", conf.oidcToken)
-}
-
-func TestOidcConfigWithTokenFromConfig(t *testing.T) {
-	p := azureNativeProvider{
-		config: map[string]string{
-			"oidcToken": "t3",
-		},
-	}
-
-	conf, err := p.determineOidcConfig()
-	assert.Nil(t, err)
-	assert.Empty(t, conf.oidcRequestToken)
-	assert.Empty(t, conf.oidcRequestUrl)
-	assert.Equal(t, "t3", conf.oidcToken)
-}
-
-func TestOidcWithTokenFileFromEnv(t *testing.T) {
-	assertConf := func(t *testing.T, p azureNativeProvider) {
-		conf, err := p.determineOidcConfig()
-		assert.Nil(t, err)
-		assert.Empty(t, conf.oidcRequestToken)
-		assert.Empty(t, conf.oidcRequestUrl)
-		assert.Empty(t, conf.oidcToken)
-		assert.Equal(t, "file1", conf.oidcTokenFilePath)
-	}
-
-	t.Run("FromEnv", func(t *testing.T) {
-		p := azureNativeProvider{}
-		t.Setenv("ARM_OIDC_TOKEN_FILE_PATH", "file1")
-		assertConf(t, p)
-	})
-
-	t.Run("FromConf", func(t *testing.T) {
-		p := azureNativeProvider{
-			config: map[string]string{
-				"oidcTokenFilePath": "file1",
-			},
-		}
-		assertConf(t, p)
-	})
-}
-
-func TestOidcEmptyConfig(t *testing.T) {
-	p := azureNativeProvider{}
-
-	t.Setenv("ARM_OIDC_TOKEN", "")
-	t.Setenv("ARM_OIDC_TOKEN_FILE_PATH", "")
-	t.Setenv("ARM_OIDC_REQUEST_TOKEN", "")
-	t.Setenv("ARM_OIDC_REQUEST_URL", "")
-	t.Setenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN", "")
-	t.Setenv("ACTIONS_ID_TOKEN_REQUEST_URL", "")
-
-	_, err := p.determineOidcConfig()
-	assert.Error(t, err)
-}
-
-func TestOidcUrlTokenPairValidation(t *testing.T) {
-	p := azureNativeProvider{}
-
-	// With a request token we also need a request URL.
-	t.Setenv("ARM_OIDC_REQUEST_TOKEN", "t1")
-	t.Setenv("ARM_OIDC_REQUEST_URL", "")
-	t.Setenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN", "")
-	t.Setenv("ACTIONS_ID_TOKEN_REQUEST_URL", "")
-
-	_, err := p.determineOidcConfig()
-	assert.Error(t, err)
-}
-
-func TestOidcPrefersToken(t *testing.T) {
-	p := azureNativeProvider{
-		config: map[string]string{
-			"oidcRequestToken": "t1",
-			"oidcRequestUrl":   "url1",
-			"oidcToken":        "t2",
-		},
-	}
-
-	config, err := p.determineOidcConfig()
-	assert.Nil(t, err)
-	assert.NotEmpty(t, config.oidcToken)
-}
-
-// For the CLI authentication method, getAuthConfig() will shell out to the `az` CLI program to determine the
+// For the CLI authentication method, readAuthConfig() will shell out to the `az` CLI program to determine the
 // cloud and subscription. This makes the test slower and will fail if `az` is not installed. In addition,
 // go-azure-helpers only allows using `az` with user authentication, not service principal, which azure/login
 // doesn't support. Therefore, we don't test the CLI authentication method by default. To test it locally, set
@@ -235,22 +99,20 @@ func TestAuthConfigs(t *testing.T) {
 		t.Run(authMethod+" is valid", func(t *testing.T) {
 			p := f()
 
-			conf, err := p.getAuthConfig()
+			conf, err := p.readAuthConfig()
 			require.NoError(t, err)
 			require.NotNil(t, conf)
 
-			assert.Equal(t, authMethod == "cli", conf.useCli)
-
-			assert.Equal(t, "public", conf.Environment)
+			assert.Equal(t, azcloud.AzurePublic, conf.cloud)
 		})
 
 		t.Run(authMethod+" public env", func(t *testing.T) {
 			p := f()
 			p.config["environment"] = "public"
 
-			conf, err := p.getAuthConfig()
+			conf, err := p.readAuthConfig()
 			require.NoError(t, err)
-			assert.Equal(t, "public", conf.Environment)
+			assert.Equal(t, azcloud.AzurePublic, conf.cloud)
 		})
 
 		t.Run(authMethod+" usgov env", func(t *testing.T) {
@@ -259,126 +121,16 @@ func TestAuthConfigs(t *testing.T) {
 
 			// The CLI auth method requires that the user switches `az` to the desired cloud/environment manually.
 			// It will fail here and we assert an appropriate error.
-			conf, err := p.getAuthConfig()
+			conf, err := p.readAuthConfig()
 			if authMethod == "cli" {
 				require.Error(t, err)
 				assert.Contains(t, err.Error(), "configured Azure environment 'usgovernment' does not match the determined environment 'public'")
 			} else {
 				require.NoError(t, err)
-				assert.Equal(t, "usgovernment", conf.Environment)
+				assert.Equal(t, azcloud.AzureGovernment, conf.cloud)
 			}
 		})
 	}
-}
-
-func TestGetAuthConfigCloud(t *testing.T) {
-	t.Run("Default is public", func(t *testing.T) {
-		p := azureNativeProvider{
-			config: map[string]string{
-				"useMsi": "true",
-			},
-		}
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzurePublic, conf.cloud())
-	})
-
-	t.Run("Public", func(t *testing.T) {
-		p := azureNativeProvider{
-			config: map[string]string{
-				"environment": "public",
-				"useMsi":      "true",
-			},
-		}
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzurePublic, conf.cloud())
-	})
-
-	t.Run("China", func(t *testing.T) {
-		p := azureNativeProvider{
-			config: map[string]string{
-				"environment": "china",
-				"useMsi":      "true",
-			},
-		}
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzureChina, conf.cloud())
-	})
-
-	t.Run("US Government", func(t *testing.T) {
-		p := azureNativeProvider{
-			config: map[string]string{
-				"environment": "usgovernment",
-				"useMsi":      "true",
-			},
-		}
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzureGovernment, conf.cloud())
-	})
-
-	t.Run("Public from environment variable", func(t *testing.T) {
-		p := azureNativeProvider{}
-		t.Setenv("ARM_ENVIRONMENT", "public")
-		t.Setenv("ARM_USE_MSI", "true")
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzurePublic, conf.cloud())
-	})
-
-	t.Run("China from environment variable", func(t *testing.T) {
-		p := azureNativeProvider{}
-		t.Setenv("ARM_ENVIRONMENT", "china")
-		t.Setenv("ARM_USE_MSI", "true")
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzureChina, conf.cloud())
-	})
-
-	t.Run("US Government from environment variable", func(t *testing.T) {
-		p := azureNativeProvider{}
-		t.Setenv("ARM_ENVIRONMENT", "usgovernment")
-		t.Setenv("ARM_USE_MSI", "true")
-		conf, err := p.getAuthConfig()
-		require.NoError(t, err)
-		assert.Equal(t, cloud.AzureGovernment, conf.cloud())
-	})
-}
-
-func TestGetCloud(t *testing.T) {
-	t.Run("Public", func(t *testing.T) {
-		a := authConfig{
-			Config: &authentication.Config{
-				Environment: "public",
-			},
-		}
-		assert.Equal(t, cloud.AzurePublic, a.cloud())
-	})
-
-	t.Run("China", func(t *testing.T) {
-		a := authConfig{
-			Config: &authentication.Config{
-				Environment: "china",
-			},
-		}
-		assert.Equal(t, cloud.AzureChina, a.cloud())
-	})
-
-	t.Run("US Government", func(t *testing.T) {
-		a := authConfig{
-			Config: &authentication.Config{
-				Environment: "usgov",
-			},
-		}
-		assert.Equal(t, cloud.AzureGovernment, a.cloud())
-	})
-
-	t.Run("Unknown", func(t *testing.T) {
-		a := authConfig{}
-		assert.Equal(t, cloud.AzurePublic, a.cloud())
-	})
 }
 
 func TestReadAzureEnvironmentFromConfig(t *testing.T) {
