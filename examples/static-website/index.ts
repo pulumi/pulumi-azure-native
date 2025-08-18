4d22a1a7541ade12f7429b06f767fe2641b09549
Bryce Lampe <bryce@pulumi.com>
2025-08-18 12:57:02 -0700

    Remove deprecated CDN resource from example

--- examples/static-website/index.ts
+++ examples/static-website/index.ts
@@ -3,7 +3,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import { URL } from "url";
 
-import * as cdn from "@pulumi/azure-native/cdn";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 
@@ -64,28 +63,3 @@ new storage.Blob("append-blob.dat", {
 
 // Web endpoint to the website
 export const staticEndpoint = storageAccount.primaryEndpoints.web;
-const staticHostname = staticEndpoint.apply(url => new URL(url).hostname);
-
-// We can add a CDN in front of the website
-const profile =  new cdn.Profile("website-cdn", {
-    resourceGroupName: resourceGroup.name,
-    location: "global",
-    sku: {
-          name: cdn.SkuName.Standard_Microsoft,
-    },
-});
-
-const endpoint = new cdn.Endpoint("website-cdn-ep", {
-    resourceGroupName: resourceGroup.name,
-    location: "westus",
-    profileName: profile.name,
-    originHostHeader: staticHostname,
-    origins: [{
-        name: "blobstorage",
-        hostName: staticHostname,
-    }],        
-});
-
-// CDN endpoint to the website.
-// Allow it some time after the deployment to get ready.
-export const cdnEndpoint = pulumi.interpolate`https://${endpoint.hostName}/`;
