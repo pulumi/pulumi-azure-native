b26648bbcf0ef03c633aff8cbb115d078b0508d2
Bryce Lampe <bryce@pulumi.com>
2025-10-06 16:07:15 -0700

    fix servicefabric path

--- provider/pkg/openapi/discover.go
+++ provider/pkg/openapi/discover.go
@@ -516,9 +516,9 @@ var excludeRegexes = []*regexp.Regexp{
 	// It's old, preview, and not important - so exclude the files of this version.
 	regexp.MustCompile(".*frontdoor/resource-manager/Microsoft.Network/preview/2018-08-01-preview.*"),
 	// This version conflicts with the managed folder version:
-	// servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/preview/2023-11-01-preview
+	// servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/ServiceFabric/preview/2023-11-01-preview
 	// This causes a conflict in the version-specific folder, not the default version folder, so we have to completely exclude it.
-	regexp.MustCompile(".*servicefabric/resource-manager/Microsoft.ServiceFabric/preview/2023-11-01-preview.*"),
+	regexp.MustCompile(".*servicefabric/resource-manager/Microsoft.ServiceFabric/ServiceFabric/preview/2023-11-01-preview.*"),
 	// This preview version is invalid OpenAPI JSON, reading it fails with encoding/json.UnmarshalTypeError in field "definitions".
 	regexp.MustCompile(".*network/resource-manager/Microsoft.Network/preview/2023-03-01-preview.*"),
 	// This version defines two "CustomRule" types so we ignore it.
