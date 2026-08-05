b9e5c0ddeac8b84d78ffa2ec581291adfe6b62ca
Ian Wahbe <me@iwahbe.com>
2026-08-05 16:58:24 +0200

    Update pulumi/pulumi sdk to v3.256.0 in the Go SDK go.mod template
    Downstream codegen will depend on a change first released in pulumi
    v3.256.0, so the go.mod files emitted into pulumi-azure-native-sdk must
    require at least that version. The pin lives in the goModTemplate of
    pulumi-gen-azure-native and takes effect at the next SDK release.

--- provider/cmd/pulumi-gen-azure-native/main.go
+++ provider/cmd/pulumi-gen-azure-native/main.go
@@ -324,7 +324,7 @@ require (
 {{ if ne .SubmoduleName "" }}
 	github.com/pulumi/pulumi-azure-native-sdk{{ .ModuleVersionPath }} {{ .Version }}
 {{ end }}
-	github.com/pulumi/pulumi/sdk/v3 v3.253.0
+	github.com/pulumi/pulumi/sdk/v3 v3.256.0
 )
 
 {{ if ne .SubmoduleName "" }}
